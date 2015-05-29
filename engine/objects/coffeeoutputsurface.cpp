f957118dc4ee20cef8182e8688d2bd92bfb0fc7e
hbirchtree <howy@ovi.com>
2015-05-29 17:59:14 +0200

    - Debugging a black screen with the new indexed buffers

--- engine/objects/coffeeoutputsurface.cpp
+++ engine/objects/coffeeoutputsurface.cpp
@@ -57,6 +57,7 @@ void CoffeeOutputSurface::render()
 
     glBindVertexArray(vao);
     glDrawArrays(GL_TRIANGLES,0,6);
+
     glBindTexture(GL_TEXTURE_2D,0);
     glBindVertexArray(0);
     glUseProgram(0);
