195953110a62c5dcd2bed45544be4dc4fbc10643
hbirchtree <howy@ovi.com>
2015-08-28 15:58:59 +0200

    - Namespace

--- coffee.h
+++ coffee.h
@@ -4,13 +4,65 @@
 #include <QDebug>
 
 namespace Coffee{
-
+//Core types, might become base type for all classes in place of QObject
 class CObject;
+
+//Wrappers of data types
+namespace CPrimitiveDataTypes{
+class ScalarValue;
+
+class Vector2Value;
+class Vector3Value;
+class Vector4Value;
+
+class QuatValue;
+
+class Matrix3Value;
+class Matrix4Value;
+}
+
+//Wrappers for GL features
+namespace CGraphicsWrappers{
+class CBuffer;
+class CVertexArrayObject;
+
+class CUniformBlock;
+class CUniformValue; //Bound to a uniform or uniform block field
+class CShader;
+
+class CFramebuffer;
+}
+
+//Physics-related types
+namespace CPhysicsSystem{
+class CPhyObject;
+
+//Physics libraries here, should make an interface soon
+class CBulletSystem;
+}
+
+//Misc. resources in engine, should not interface with GL directly
+namespace CResourceTypes{
+class CMesh;
+class CTexture;
+class CInstanceContainer;
+
+class CCamera;
+class CMaterial;
+class CLight;
+
+//Separating animation-related types
+namespace CAnimationDataTypes{
+class CSkeleton;
+class CAnimationState;
+}
+}
 
 //Core classes
 namespace CFunctional{
 class CRQueueRunner;
 
+//Templates for function wrappers, all under a common CRFunction type
 template<typename RType, typename... AType>
 class CRFunction;
 template<typename RType,typename... AType>
@@ -22,6 +74,7 @@ typedef std::function<void()> QueueFunction; //This needs to be light
 
 }
 
+//Input classes
 namespace CInput{
 typedef CFunctional::CRFunction<void,void*,uint32_t> CInputHandlerFunction;
 
@@ -37,19 +90,25 @@ struct CIJoyState;
 class CIEventParser;
 }
 
+//Windowing and rendering contexts
 namespace CDisplay{
 class CDRendererBase;
 class CDRenderer;
+
+class CGLFWRenderer;
+class CGPUMonitor;
 }
 
+//File management
 namespace CFiles{
-class CFResource;
+class CResource;
 class CFAssetStorage;
 class CFObjectImport;
 }
 
+//Scripting
 namespace CScripting{
-class CSScriptEngine;
+class CScriptEngine;
 class CSObject;
 }
 
