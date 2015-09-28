26ffbc0b64f0744522cf81fd747a949fd4fe1e13
hbirchtree <howy@ovi.com>
2015-09-28 16:14:43 +0200

    - More SWIG

--- swig/CoffeeSharp/CoffeeSharp/CoffeeSharpTest.cs
+++ swig/CoffeeSharp/CoffeeSharp/CoffeeSharpTest.cs
@@ -11,11 +11,17 @@ namespace CoffeeSharp
 		public static void Main(String[] args)
 		{
 			Console.WriteLine ("Application directory: {0}\n" +
-			                   "Application executable: {1}\n" +
+			                   "Current directory: {1}\n" +
 			                   "Userdata directory: {2}",
 			                   Coffee.coffee_get_application_dir(),
-			                   Coffee.coffee_get_env_variable("HOME"),
+			                   Coffee.coffee_get_current_dir(),
 			                   Coffee.coffee_get_userdata_dir("hbirchtree","Coffee of Time"));
+
+			CResource res = new CResource ("ubw/models/ubw.fbx");
+			res.read_data ();
+			CAssimpData meshes = CAssimpImporters.importResource (res,res.resource());
+
+			Console.WriteLine ("Mesh data: {0}",meshes.numMeshes);
 
 			CGLContextVersion ctxtVer = new CGLContextVersion (3,3);
 
