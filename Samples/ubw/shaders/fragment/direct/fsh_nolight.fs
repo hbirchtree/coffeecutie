362ec68da8fa2c7fd8d1dce5068b0ad84962faa8
hbirchtree <howy@ovi.com>
2015-09-29 14:27:38 +0200

    - Shaders now work with Intel again

--- Samples/ubw/shaders/fragment/direct/fsh_nolight.fs
+++ Samples/ubw/shaders/fragment/direct/fsh_nolight.fs
@@ -1,32 +1,29 @@
-#version 410
-#extension GL_ARB_bindless_texture : require
-#extension GL_ARB_shader_storage_buffer_object : require
-#extension GL_ARB_shader_image_load_store : require
+#version 330
 
-in v_block {
-	vec3 vert;
-	vec4 mVert;
-	vec2 texCoord;
-	vec3 normal;
-	vec3 wNormal;
-	vec3 wTangent;
-	flat int iDrawID;
-} In;
+uniform mat4 camera;
+uniform mat4 model;
 
 // material settings
-layout(std430, binding=1) buffer sampler_data {
-	vec3 diffuseSampler[];
-};
+uniform struct Material {
+	sampler2D diffuseSampler;
 
-layout(location = 0) out vec4 Out_color;
+	vec3 transparencyValue;
+	vec3 colorMultiplier;
+} mtl;
+
+in vec2 fragTexCoord;
+in vec3 fragNormal;
+in vec3 fragVert;
+
+out vec4 finalColor;
 
 void main() {
-//    vec4 surfaceColor = texture(mtl.diffuseSampler, fragTexCoord);
+    vec4 surfaceColor = texture(mtl.diffuseSampler, fragTexCoord);
 
     //diffuse
-//    vec3 diffuse = surfaceColor.rgb*mtl.colorMultiplier;
-
+    vec3 diffuse = surfaceColor.rgb*mtl.colorMultiplier;
+    
     //final color (after gamma correction)
-//    vec3 gamma = vec3(1.0/1.3);
-    Out_color = vec4(diffuseSampler[In.iDrawID],1.0);
+    vec3 gamma = vec3(1.0/1.3);
+    finalColor = vec4(pow(diffuse, gamma), surfaceColor.a);
 }
