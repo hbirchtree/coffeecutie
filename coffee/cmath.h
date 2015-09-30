3bd83db26aa4e354153e29a6f162fc0c952fb7d4
Håvard Bjerke <h0wy36@gmail.com>
2015-09-30 11:29:08 +0200

    - Added CMath

---
+++ coffee/cmath.h
@@ -0,0 +1,25 @@
+#ifndef CMATH_FUN
+#define CMATH_FUN
+
+#include <cmath>
+#include "coffee_types.h"
+
+#include <glm/glm/glm.hpp>
+#include <glm/glm/gtc/type_ptr.hpp>
+#include <glm/glm/gtc/matrix_transform.hpp>
+
+/*
+ * Doing this, we can replace individual functions depending on platform or other critera
+ */
+
+namespace Coffee{
+namespace CMath{
+
+using namespace std;
+
+using namespace glm;
+
+}
+}
+
+#endif
