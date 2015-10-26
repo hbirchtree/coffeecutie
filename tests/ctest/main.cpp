7968e9f4104753d854d45eb49928bdc393484110
Håvard Bjerke <h0wy36@gmail.com>
2015-10-26 03:53:54 +0100

    - Re-enable multi-threaded running

--- tests/ctest/main.cpp
+++ tests/ctest/main.cpp
@@ -61,20 +61,20 @@ int main(int,char**)
     std::atomic<ubyte> atomic;
     atomic.store(0);
     CThreading::CThreadWorker<ubyte> worker(atomic);
-//    std::future<void> ret = worker.run([=](){
-//        try{
+    std::future<void> ret = worker.run([=](){
+        try{
             renderer->run(props);
-//        }catch(std::runtime_error exc){
-//            cDebug("Caught exception in thread: 0x%llx, message: %s",std::this_thread::get_id(),exc.what());
-//            worker.dataPtr()->store(1);
-//            renderer->cleanup();
-//        }
-//        if(worker.dataPtr()->load()==0)
-//            worker.dataPtr()->store(255);
-//    });
+        }catch(std::runtime_error exc){
+            cDebug("Caught exception in thread: 0x%llx, message: %s",std::this_thread::get_id(),exc.what());
+            worker.dataPtr()->store(1);
+            renderer->cleanup();
+        }
+        if(worker.dataPtr()->load()==0)
+            worker.dataPtr()->store(255);
+    });
 
-//    while(worker.dataPtr()->load()==0)
-//        std::this_thread::sleep_for(std::chrono::milliseconds(5));
+    while(worker.dataPtr()->load()==0)
+        std::this_thread::sleep_for(std::chrono::milliseconds(5));
 
     switch(worker.dataPtr()->load()){
     case 1:
