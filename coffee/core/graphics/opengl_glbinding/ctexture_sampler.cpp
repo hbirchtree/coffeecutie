a1dbda34b4e8282937333ff3026afc9fbabdb048
Håvard Bjerke <h0wy36@gmail.com>
2015-12-10 04:47:22 +0100

    - Add missing files

---
+++ coffee/core/graphics/opengl_glbinding/ctexture_sampler.cpp
@@ -0,0 +1,79 @@
+#include "ctexture_sampler.h"
+
+#include "glbinding.h"
+
+namespace Coffee{
+namespace CGraphicsWrappers{
+
+void coffee_graphics_alloc(CTextureSampler *sampler)
+{
+    glCreateSamplers(1,&sampler->handle);
+}
+
+void coffee_graphics_free(CTextureSampler *sampler)
+{
+    glDeleteSamplers(1,&sampler->handle);
+}
+
+void coffee_graphics_tex_parami(
+        CTextureSampler &tex, CTexParam const& param, CGint const& val)
+{
+    glSamplerParameteri(tex.handle,
+                        gl_get(param),
+                        val);
+}
+
+void coffee_graphics_tex_paramf(
+        CTextureSampler &tex, CTexParam const& param, scalar const& val)
+{
+    glSamplerParameterf(
+                tex.handle,
+                gl_get(param),
+                val);
+}
+
+void coffee_graphics_tex_param(
+        CTextureSampler &tex, CTexParam const& param, CTexParamOpt const& val)
+{
+    coffee_graphics_tex_parami(tex,param,(CGint)gl_get(val));
+}
+
+CGhnd64 coffee_graphics_tex_get_handle(CTexture const& texture,CTextureSampler &samp)
+{
+    samp.bhandle = glGetTextureSamplerHandleARB(texture.handle,samp.handle);
+    return samp.bhandle;
+}
+
+void coffee_graphics_tex_make_resident(const CTextureSampler &samp)
+{
+    glMakeTextureHandleResidentARB(samp.bhandle);
+}
+
+void coffee_graphics_tex_make_nonresident(const CTextureSampler &samp)
+{
+    glMakeTextureHandleNonResidentARB(samp.bhandle);
+}
+
+void coffee_graphics_tex_load(CTextureSampler const& tex, const CTexture &)
+{
+    coffee_graphics_tex_make_resident(tex);
+}
+
+void coffee_graphics_tex_unload(CTextureSampler const& tex, const CTexture &)
+{
+    coffee_graphics_tex_make_nonresident(tex);
+}
+
+void coffee_graphics_tex_load_safe(CTextureSampler const& sampler, const CTexture &tex)
+{
+    glActiveTexture(GL_TEXTURE0+sampler.unit);
+    glBindTexture(gl_get(tex.textureType),tex.handle);
+    glBindSampler(sampler.unit,sampler.handle);
+}
+
+void coffee_graphics_tex_unload_safe(CTextureSampler const&, CTexture const&)
+{
+}
+
+}
+}
---
+++ coffee/core/graphics/opengl_glbinding/ctexture_sampler.h
@@ -0,0 +1,76 @@
+#ifndef COFFEE_GRAPHICS_OPENGL_TEXTURE_SAMPLER_H
+#define COFFEE_GRAPHICS_OPENGL_TEXTURE_SAMPLER_H
+
+#include "copengl_types.h"
+#include "ctexture_types.h"
+
+namespace Coffee{
+namespace CGraphicsWrappers{
+/*!
+ * \brief Allocate texture sampler
+ * \param sampler
+ */
+extern void coffee_graphics_alloc(CTextureSampler* sampler);
+/*!
+ * \brief Free texture sampler
+ * \param sampler
+ */
+extern void coffee_graphics_free(CTextureSampler* sampler);
+/*!
+ * \brief Get texture handle for a texture-sampler pair
+ * \param texture Texture target
+ * \param samp Sampler target
+ * \return The handle of the created pair. Will return the same handle if it is already acquired.
+ */
+extern CGhnd64 coffee_graphics_tex_get_handle(const CTexture &texture, CTextureSampler &samp);
+/*!
+ * \brief Make a texture resident for sampling
+ * \param samp
+ */
+extern void coffee_graphics_tex_make_resident(CTextureSampler const& samp);
+/*!
+ * \brief Move texture out of residency
+ * \param samp
+ */
+extern void coffee_graphics_tex_make_nonresident(CTextureSampler const& samp);
+
+/*!
+ * \brief Set texture parameter, GLint variant
+ * \param tex
+ * \param param
+ * \param val
+ */
+extern void coffee_graphics_tex_sampler_parami(
+        CTextureSampler &tex, CTexParam const& param, CGint const& val);
+
+/*!
+ * \brief Set texture parameter, GLfloat variant
+ * \param tex
+ * \param param
+ * \param val
+ */
+extern void coffee_graphics_tex_sampler_paramf(
+        CTextureSampler &tex, CTexParam const& param, scalar const& val);
+/*!
+ * \brief Set texture parameter, GLenum variant
+ * \param tex
+ * \param param
+ * \param val
+ */
+extern void coffee_graphics_tex_sampler_parame(
+        CTextureSampler &tex, CTexParam const& param, CTexParamOpt const& val);
+
+extern void coffee_graphics_tex_load(
+        CTextureSampler const& tex, CTexture const&);
+
+extern void coffee_graphics_tex_unload(CTextureSampler const& tex, CTexture const&);
+
+extern void coffee_graphics_tex_load_safe(
+        CTextureSampler const& sampler, CTexture const& tex);
+
+extern void coffee_graphics_tex_unload_safe(CTextureSampler const&, CTexture const&);
+
+}
+}
+
+#endif
