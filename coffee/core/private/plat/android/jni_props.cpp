655a921dd0979bd4ba80bcd03186f662e6e5919f
hbirchtree <howy@ovi.com>
2016-01-14 21:10:05 +0100

    - Add Android building files

--- coffee/core/private/plat/android/jni_props.cpp
+++ coffee/core/private/plat/android/jni_props.cpp
@@ -5,4 +5,28 @@
 #include <jni.h>
 #include <android/log.h>
 
+namespace Coffee{
+
+cstring GetInternalStorageDir()
+{
+    return nullptr;
+}
+
+cstring GetExternalStorageDir()
+{
+    return nullptr;
+}
+
+cstring GetUserDataDir()
+{
+    return nullptr;
+}
+
+cstring GetAppName()
+{
+    return nullptr;
+}
+
+}
+
 #endif
