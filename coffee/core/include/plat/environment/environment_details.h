40f49d482eef59952f5ea2a47cad8dc62232f916
Håvard Bjerke <h0wy36@gmail.com>
2016-02-06 18:21:03 +0100

    - Add Windows header, not implemented

--- coffee/core/include/plat/environment/environment_details.h
+++ coffee/core/include/plat/environment/environment_details.h
@@ -28,31 +28,9 @@ struct EnvInterface
     static CString CurrentDir();
 };
 
-//namespace Env{
-
-//extern cstring_w GetVar(cstring var);
-
-//extern bool SetVar(cstring var, cstring value);
-
-//extern bool UnsetVar(cstring var);
-
-//extern bool ClearEnv();
-
-//extern cstring GetPathSep();
-
-//extern cstring_w ConcatPath(cstring_w target, cstring v2);
-
-//extern cstring_w GetUserHome();
-
-//extern cstring_w GetUserData(cstring orgname, cstring appname);
-
-//extern cstring_w ApplicationDir();
-
-//extern cstring_w CurrentDir();
-
-//}
 } //Coffee
 
 #endif
 
 #include "unix/environment.h"
+#include "windows/environment.h"
