a15b61833e33de897b2fc931e1d599c48c42136b
hbirchtree <h0wy36@gmail.com>
2016-03-14 10:43:58 +0100

    - Make ARB_cull_distance optional in base-test

--- sample_data/vr/vshader.glsl
+++ sample_data/vr/vshader.glsl
@@ -1,4 +1,5 @@
 #version 330 core
+#extension ARB_cull_distance : enable
 
 layout(location=0)in vec3 pos;
 layout(location=1)in vec2 tex;
@@ -6,7 +7,9 @@ layout(location=1)in vec2 tex;
 out gl_PerVertex{
     vec4 gl_Position;
     float gl_ClipDistance[1];
+#ifdef ARB_cull_distance
     float gl_CullDistance[1];
+#endif
 };
 out VS_OUT{
     vec2 tc;
@@ -36,6 +39,8 @@ void main(void)
     /* This lets us avoid having to fiddle with viewports or viewport arrays */
     gl_ClipDistance[0] = dot(gl_Position,clipPlane);
 
+#ifdef ARB_cull_distance
     /* TODO: Fix culling, this could be important for perf */
     gl_CullDistance[0] = 1.0;
+#endif
 }
