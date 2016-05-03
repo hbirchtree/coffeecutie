8c28274429e770e7153290ff73fef476fd76faa0
Håvard Bjerke <h0wy36@gmail.com>
2016-05-03 02:52:16 +0200

    - Add more features to buildserver

--- tools/build-server/bserver.py
+++ tools/build-server/bserver.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 from flask import Flask
+from flask import request
 
 # Create an instance of Flask
 app = Flask(__name__);
@@ -11,9 +12,11 @@ def default_route():
     return "Welcome to the Coffee Build Server, where you may read error reports!";
 
 # For servers, get their logs and put them in the database
-@app.route("/logger/data/<string:arch>/<int:sid>")
+@app.route("/logger/data/<arch>/<int:sid>",methods=["POST","GET"])
 def build_log_data(arch,sid):
-    return "OMG! Someone messaged me!";
+    if request.method == "GET":
+        return "Are you lost?";
+    return "You are: plat=%s,server_id=%s" % (arch,sid);
 
 if __name__ == "__main__":
     app.run();
