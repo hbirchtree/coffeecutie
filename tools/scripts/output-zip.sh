8e39324dcf7ffcbb8dfc5870f57affbed8f2d215
Håvard Bjerke <h0wy36@gmail.com>
2016-05-13 01:57:42 +0200

    - Add script for packaging to zip file

---
+++ tools/scripts/output-zip.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+function check_input()
+{
+    if [[ -z "$1" ]]; then
+        printf "Build directory not specified!\n" 1>&2;
+        exit 1;
+    fi
+    if [[ ! -d "$1" ]]; then
+        printf "Valid build directory not specified!\n" 1>&2;
+        exit 1;
+    fi
+}
+
+check_input "$1";
+
+if [[ -f "$1/build.ninja" ]]; then
+    ninja -C "$1" install 1>&2
+elif [[ -f "$1/Makefile" ]]; then
+    make -C "$1" install 1>&2
+fi
+
+cd "$1"
+
+zip -9 -r - "out" | base64 -w 0
