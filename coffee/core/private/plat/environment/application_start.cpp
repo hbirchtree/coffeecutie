9b14ebdff3334dde308052390315b4418cff16f9
hbirchtree <h0wy36@gmail.com>
2016-05-13 17:10:03 +0200

    - Hide console window on Windows, it looked really ugly

--- coffee/core/private/plat/environment/application_start.cpp
+++ coffee/core/private/plat/environment/application_start.cpp
@@ -1,9 +1,16 @@
 #include <coffee/core/CApplication>
 #include <coffee/core/coffee.h>
 
+#if defined(COFFEE_WINDOWS)
+#include <coffee/core/plat/plat_windows.h>
+#endif
+
 using namespace Coffee;
 
 int deref_main(CoffeeMainWithArgs mainfun, int argc, char** argv)
 {
+#if defined(COFFEE_WINDOWS)
+	ShowWindow(GetConsoleWindow(), SW_HIDE);
+#endif
     return Coffee::CoffeeMain(mainfun,argc,argv);
 }
