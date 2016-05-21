eda6ac54678ee3d22a3686ec00df718cf9cb810e
Håvard Bjerke <h0wy36@gmail.com>
2016-05-21 02:20:50 +0200

    - Fixes to Windows library loading

--- include/coffee/core/plat/linking/windows/loader.h
+++ include/coffee/core/plat/linking/windows/loader.h
@@ -7,24 +7,49 @@
 #include "../libraries.h"
 
 namespace Coffee {
-	namespace Library {
-		namespace Windows {
+namespace Library {
+namespace Windows {
 
-			struct WindowsFunctionLoader : FunctionLoad_def
-			{
+struct WindowsFunctionLoader : FunctionLoad_def
+{
+    struct Library
+    {
+        HINSTANCE libraryHandle;
+    };
 
-			};
-			struct WindowsObjectLoader : ObjectLoader_def<WindowsFunctionLoader>
-			{
+    Library* GetLibrary(
+            cstring name,
+            LoadFlags lfl = NoFlags,
+            Version const* ver = nullptr,
+            CString* = nullptr)
+    {
+        Library* l = new Library;
 
-			};
+        CString libname = name;
+        name += ".dll";
 
-		}
+        HINSTANCE hnd = LoadLibrary(libname.c_str());
 
-		using FunctionLoader = Windows::WindowsFunctionLoader;
-		using ObjectLoader = Windows::WindowsObjectLoader;
+        if(!l->libraryHandle)
+        {
+            delete l;
+            return nullptr;
+        }
 
-	}
+        return l;
+    }
+};
+struct WindowsObjectLoader : ObjectLoader_def<WindowsFunctionLoader>
+{
+
+};
+
+}
+
+using FunctionLoader = Windows::WindowsFunctionLoader;
+using ObjectLoader = Windows::WindowsObjectLoader;
+
+}
 }
 
 #endif
