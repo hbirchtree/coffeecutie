fc2eadcd16dc4b71a072edbf3f29b9fe6094d015
hbirchtree <h0wy36@gmail.com>
2016-05-24 15:54:38 +0200

    - Fully implement win32 library loading

--- include/coffee/core/plat/linking/windows/loader.h
+++ include/coffee/core/plat/linking/windows/loader.h
@@ -12,12 +12,15 @@ namespace Windows {
 
 struct WindowsFunctionLoader : FunctionLoad_def
 {
+	static const constexpr cstring LIBRARY_SUFFIX = ".dll";
+
     struct Library
     {
         HINSTANCE libraryHandle;
     };
 
-    Library* GetLibrary(
+	STATICINLINE
+	Library* GetLibrary(
             cstring name,
             LoadFlags lfl = NoFlags,
             Version const* ver = nullptr,
@@ -26,7 +29,7 @@ struct WindowsFunctionLoader : FunctionLoad_def
         Library* l = new Library;
 
         CString libname = name;
-        name += ".dll";
+        libname += LIBRARY_SUFFIX;
 
         HINSTANCE hnd = LoadLibrary(libname.c_str());
 
@@ -38,10 +41,47 @@ struct WindowsFunctionLoader : FunctionLoad_def
 
         return l;
     }
+
+	STATICINLINE
+	bool UnloadLibrary(
+			Library* lib,
+			CString* = nullptr)
+	{
+		if (lib->libraryHandle)
+		{
+			FreeLibrary(lib->libraryHandle);
+			delete lib;
+			return true;
+		}
+		return false;
+	}
+
+	template<typename RType, typename... AType>
+	struct Loader : FunctionLoad_def::Loader<RType,AType...>
+	{
+		STATICINLINE
+		Fun GetFunction(
+				Library* lib,
+				cstring funcname,
+				CString* = nullptr)
+		{
+			return (Fun)GetProcAddress(lib->libraryHandle,funcname);
+		}
+	};
 };
 struct WindowsObjectLoader : ObjectLoader_def<WindowsFunctionLoader>
 {
+	using Loader = WindowsFunctionLoader;
 
+	template<typename Obj,typename... Args> STATICINLINE
+		ObjConstructor<Obj> GetConstructor(
+			Loader::Library* library,
+			cstring constructor,
+			CString* err = nullptr
+		)
+	{
+		return{ Loader::Loader<Obj*,Args...>::GetFunction(library,constructor,err)};
+	}
 };
 
 }
