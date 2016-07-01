dfc23e44e82226d8d1b3b0d5a55e911dead49ca3
Håvard Bjerke <h0wy36@gmail.com>
2016-07-01 04:23:37 +0200

    - Add output to profiler data for function benchmark

--- examples/unit-tests/function-tests/test01.cpp
+++ examples/unit-tests/function-tests/test01.cpp
@@ -28,59 +28,70 @@ bool function_test()
 
         int32 v1 = 1,v2 = 2;
 
+        Profiler::Profile("Setup");
+
         /* Virtual function, base class */
         t.start();
         for(i=0;i<iterations;i++)
             t1.fun();
         time_1_1 = t.elapsed();
+        Profiler::Profile("Virtual, base class");
 
         /* Non-virtual function, base class */
         t.start();
         for(i=0;i<iterations;i++)
             t1.directfun();
         time_1_2 = t.elapsed();
+        Profiler::Profile("Non-virtual, base class");
 
         /* Virtual overridden function */
         t.start();
         for(i=0;i<iterations;i++)
             t2.fun();
         time_2 = t.elapsed();
+        Profiler::Profile("Virtual overridden, base class");
 
         /* Extern virtual overridden function */
         t.start();
         for(i=0;i<iterations;i++)
             t3.fun();
         time_3_1 = t.elapsed();
+        Profiler::Profile("External virtual overridden, base class");
 
         /* Extern base function */
         t.start();
         for(i=0;i<iterations;i++)
             t3.directfun();
         time_3_2 = t.elapsed();
+        Profiler::Profile("External, base class");
 
         /* Direct function, inlined */
         t.start();
         for(i=0;i<iterations;i++)
             StaticCall(v1,v2);
         time_4_1 = t.elapsed();
+        Profiler::Profile("Inlined direct, no class");
 
         /* Static function, inlined */
         t.start();
         for(i=0;i<iterations;i++)
             TestStaticClass::StaticClassCall(v1,v2);
         time_4_2 = t.elapsed();
+        Profiler::Profile("Static inlined, base class");
 
         /* Static function, inlined, overridden */
         t.start();
         for(i=0;i<iterations;i++)
             TestStaticOverrideClass::StaticClassCall(v1,v2);
         time_4_3 = t.elapsed();
+        Profiler::Profile("Static overridden inline, base class");
 
         /* External function */
         t.start();
         for(i=0;i<iterations;i++)
             ExternCall(v1,v2);
         time_5 = t.elapsed();
+        Profiler::Profile("External, no class");
     }
 
     Vector<uint64> timing;
