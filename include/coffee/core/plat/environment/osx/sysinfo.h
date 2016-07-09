1a6aabe670e80f2f3612add71990ae2562f03427
hbirchtree <h0wy36@gmail.com>
2016-07-09 21:31:24 +0200

    - Add system version to OSX port, very hacky

--- include/coffee/core/plat/environment/osx/sysinfo.h
+++ include/coffee/core/plat/environment/osx/sysinfo.h
@@ -7,12 +7,28 @@
 #include "../sysinfo_def.h"
 #include "../sdlpowerinfo.h"
 
+#include <stdio.h>
+
 namespace Coffee{
 namespace Environment{
 namespace Mac{
 
 struct MacSysInfo : SysInfoDef
 {
+    STATICINLINE CString GetSystemVersion()
+    {
+        FILE* out = popen("sw_vers -productVersion","r");
+        if(out)
+        {
+            char buf[16];
+            char* ptr = fgets(buf, sizeof(buf),out);
+            pclose(out);
+            CString output = ptr;
+            output.resize(Search::ChrFind((cstring)ptr,'\n')-ptr);
+            return output;
+        }else
+            return "0.0";
+    }
 };
 
 using MacPowerInfo = _SDLPowerInfo;
