85fd19e57af8f7c8f8c2e59d43112b49c148cafe
Håvard Bjerke <h0wy36@gmail.com>
2016-07-20 02:18:47 +0200

    - Disable manual .app creation for iOS, using MACOSX_BUNDLE

--- cmake/Packaging/MacAppBuild.cmake
+++ cmake/Packaging/MacAppBuild.cmake
@@ -3,9 +3,9 @@ if(APPLE)
 endif()
 
 macro( MACFRAMEWORK_PACKAGE
-	TARGET LINKOPT
-	VERSION_CODE COPYRIGHT COMPANY
-	SOURCES BUNDLE_RSRCS BUNDLE_HEADERS)
+        TARGET LINKOPT
+        VERSION_CODE COPYRIGHT COMPANY
+        SOURCES BUNDLE_RSRCS BUNDLE_HEADERS)
 
     set ( VERSION_STD_CODE "1.0.0" )
     set ( VERSION_COMP_CODE "1.0.0" )
@@ -13,27 +13,27 @@ macro( MACFRAMEWORK_PACKAGE
     add_definitions( -DCOFFEE_APPLICATION_LIBRARY )
 
     if(NOT IOS)
-        add_library("${TARGET}" SHARED "${SOURCES}")
+        add_library("${TARGET}" ${LINKOPT} "${SOURCES}")
     else()
         add_library("${TARGET}" STATIC "${SOURCES}")
     endif()
 
-    set_target_properties("${TARGET}" PROPERTIES 
+    set_target_properties("${TARGET}" PROPERTIES
         POSITION_INDEPENDENT_CODE TRUE
         FRAMEWORK TRUE
         MACOSX_FRAMEWORK_IDENTIFIER org.coffeecutie-dev.${TARGET}
         MACOSX_FRAMEWORK_SHORT_VERSION_STRING ${VERSION_CODE}
         MACOSX_FRAMEWORK_BUNDLE_VERSION ${VERSION_CODE}
-	VERSION ${VERSION_STD_CODE}
-	SOVERSION ${VERSION_COMP_CODE}
+        VERSION ${VERSION_STD_CODE}
+        SOVERSION ${VERSION_COMP_CODE}
         )
 
     # TODO: Process resource files
     # TODO: Process header files
 
     install(
-	TARGETS
-	"${TARGET}"
+        TARGETS
+        "${TARGET}"
 
         LIBRARY DESTINATION
         lib
@@ -42,10 +42,10 @@ macro( MACFRAMEWORK_PACKAGE
         lib
 
         FRAMEWORK DESTINATION
-	frameworks
+        frameworks
         COMPONENT
         bin
-	)
+        )
 endmacro()
 
 macro( MACAPP_PACKAGE
@@ -92,70 +92,74 @@ macro( MACAPP_PACKAGE
         message ( "Framework: ${fw}" )
     endforeach()
 
-    if(NOT IOS)
-        add_executable(${TARGET} MACOSX_BUNDLE
-            ${BUNDLE_FILES} ${OSX_ICON}
-            ${SOURCES}
-            )
+    #    if(NOT IOS)
+    add_executable(${TARGET} MACOSX_BUNDLE
+        ${BUNDLE_FILES} ${OSX_ICON}
+        ${SOURCES}
+        )
 
-        # Lots of properties!
-        set_target_properties(${TARGET} PROPERTIES
-            MACOSX_BUNDLE_BUNDLE_NAME "${TITLE}"
-            MACOSX_BUNDLE_GUI_IDENTIFIER "${TITLE}"
-            MACOSX_BUNDLE_ICON_FILE "${OSX_ICON_NAME}"
+    if(IOS)
 
-            MACOSX_BUNDLE_COPYRIGHT "${COPYRIGHT}"
-            MACOSX_BUNDLE_INFO_STRING "${INFO_STRING}"
-
-            MACOSX_BUNDLE_BUNDLE_VERSION "${COFFEE_BUILD_STRING}"
-            MACOSX_BUNDLE_SHORT_VERSION_STRING "${COFFEE_VERSION_CODE}"
-            MACOSX_BUNDLE_LONG_VERSION_STRING "${COFFEE_BUILD_STRING}"
-            )
-
-        install(
-            TARGETS
-            ${TARGET}
-
-            DESTINATION
-            ${CMAKE_PACKAGED_OUTPUT_PREFIX}/apple-osx
-            )
-    else()
-        set ( APP_DIR "${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/${TITLE}.app" )
-
-        set ( IOS_NAME "${TITLE}" )
-        set ( IOS_IDENTIFIER "${TITLE}" )
-        set ( IOS_INFO "${INFO_STRING}" )
-
-        set ( IOS_BUNDLEVER "${COFFEE_BUILD_STRING}" )
-        set ( IOS_SHORTVER "${COFFEE_VERSION_CODE}" )
-        set ( IOS_LONGVER "${COFFEE_BUILD_STRING}" )
-
-        set ( IOS_EXEC "${TITLE}" )
-        set ( IOS_LANG "English" )
-        set ( IOS_LAUNCH_IMG "Default")
-        set ( IOS_HIDE_STATUSBAR "true" )
-        set ( IOS_PRERENDER_ICON "true" )
-
-        set ( IOS_ORIENTATION "UIInterfaceOrientationLandscapeLeft" )
-
-        set ( IOS_SIGNATURE "..." )
-
-        add_executable ( ${TARGET} ${SOURCES} )
-
-        add_custom_command (
-            TARGET "${TARGET}"
-            POST_BUILD
-            COMMAND "${CMAKE_COMMAND}" -E make_directory "${APP_DIR}"
-            )
-        add_custom_command (
-            TARGET "${TARGET}"
-            POST_BUILD
-            COMMAND "${CMAKE_COMMAND}" -E copy "$<TARGET_FILE:${TARGET}>" "${APP_DIR}/${TITLE}"
-            )
-        configure_file (
-            "${COFFEE_DESKTOP_DIRECTORY}/osx/ios/Info.plist.xml"
-            "${APP_DIR}/Info.plist"
-            @ONLY
-            )
     endif()
+
+    # Lots of properties!
+    set_target_properties(${TARGET} PROPERTIES
+        MACOSX_BUNDLE_BUNDLE_NAME "${TITLE}"
+        MACOSX_BUNDLE_GUI_IDENTIFIER "${TITLE}"
+        MACOSX_BUNDLE_ICON_FILE "${OSX_ICON_NAME}"
+
+        MACOSX_BUNDLE_COPYRIGHT "${COPYRIGHT}"
+        MACOSX_BUNDLE_INFO_STRING "${INFO_STRING}"
+
+        MACOSX_BUNDLE_BUNDLE_VERSION "${COFFEE_BUILD_STRING}"
+        MACOSX_BUNDLE_SHORT_VERSION_STRING "${COFFEE_VERSION_CODE}"
+        MACOSX_BUNDLE_LONG_VERSION_STRING "${COFFEE_BUILD_STRING}"
+        )
+
+    install(
+        TARGETS
+        ${TARGET}
+
+        DESTINATION
+        ${CMAKE_PACKAGED_OUTPUT_PREFIX}/apple-osx
+        )
+    #    else()
+    #        set ( APP_DIR "${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/${TITLE}.app" )
+
+    #        set ( IOS_NAME "${TITLE}" )
+    #        set ( IOS_IDENTIFIER "${TITLE}" )
+    #        set ( IOS_INFO "${INFO_STRING}" )
+
+    #        set ( IOS_BUNDLEVER "${COFFEE_BUILD_STRING}" )
+    #        set ( IOS_SHORTVER "${COFFEE_VERSION_CODE}" )
+    #        set ( IOS_LONGVER "${COFFEE_BUILD_STRING}" )
+
+    #        set ( IOS_EXEC "${TITLE}" )
+    #        set ( IOS_LANG "English" )
+    #        set ( IOS_LAUNCH_IMG "Default")
+    #        set ( IOS_HIDE_STATUSBAR "true" )
+    #        set ( IOS_PRERENDER_ICON "true" )
+
+    #        set ( IOS_ORIENTATION "UIInterfaceOrientationLandscapeLeft" )
+
+    #        set ( IOS_SIGNATURE "..." )
+
+    #        add_executable ( ${TARGET} ${SOURCES} )
+
+    #        add_custom_command (
+    #            TARGET "${TARGET}"
+    #            POST_BUILD
+    #            COMMAND "${CMAKE_COMMAND}" -E make_directory "${APP_DIR}"
+    #            )
+    #        add_custom_command (
+    #            TARGET "${TARGET}"
+    #            POST_BUILD
+    #            COMMAND "${CMAKE_COMMAND}" -E copy "$<TARGET_FILE:${TARGET}>" "${APP_DIR}/${TITLE}"
+    #            )
+    #        configure_file (
+    #            "${COFFEE_DESKTOP_DIRECTORY}/osx/ios/Info.plist.xml"
+    #            "${APP_DIR}/Info.plist"
+    #            @ONLY
+    #            )
+    #    endif()
 endmacro()
