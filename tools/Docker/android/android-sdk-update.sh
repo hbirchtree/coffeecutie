d985e4e3de1eab5b3e4628182e42ed5eb05fde1d
Håvard Bjerke <h0wy36@gmail.com>
2016-07-22 15:08:59 +0200

    - Remove Dockerfiles from main repository, moved into submodule tools/automation/, can be checked out with less overhead in Jenkins

--- tools/Docker/android/android-sdk-update.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Installs latest build-tools, properly
-android update sdk -u -t 1
-# Install API levels and etc.
-android update sdk -u --filter "tools,platform-tools,android-19,android-21"
