1ce035af09aa1523c48c236ddad2dcf595aff3bc
Håvard Bjerke <h0wy36@gmail.com>
2016-10-29 03:19:51 +0200

    - Update Travis build scripts

--- tools/travis/build.sh
+++ tools/travis/build.sh
@@ -5,7 +5,7 @@ case "${TRAVIS_OS_NAME}" in
 cmake -G"Ninja" -DCMAKE_TOOLCHAIN_FILE=cmake/Toolchains/native-linux-generic.toolchain.cmake -Ccmake/Preload/x86_64-linux-generic.cmake -DCMAKE_INSTALL_PREFIX=out
 ;;
 "osx")
-cmake -G"Ninja" -DCMAKE_TOOLCHAIN_FILE=cmake/Toolchains/native-macintosh-generic.toolchain.cmake -Ccmake/Preload/x86_64-osx-generic.cmake -DOPENSSL_ROOT_DIR=/usr/local/opt/openssl -DCMAKE_INSTALL_PREFIX=out
+cmake -G"Ninja" -DCMAKE_TOOLCHAIN_FILE=cmake/Toolchains/native-macintosh-generic.toolchain.cmake -Ccmake/Preload/x86_64-osx-generic.cmake -DOPENSSL_ROOT_DIR=/usr/local/opt/openssl -DSWIG_DIR=/usr/local/opt/swig304 -DCMAKE_INSTALL_PREFIX=out
 ;;
 esac
 
--- tools/travis/install-dependencies.sh
+++ tools/travis/install-dependencies.sh
@@ -8,6 +8,6 @@ sudo apt-get install -qy libsdl2-dev libopenal-dev cmake ninja-build swig3.0
 "osx")
 brew install homebrew/versions/llvm38 --with-clang
 brew install homebrew/versions/swig304
-brew install sdl2 openal-soft cmake ninja openssl swig
+brew install sdl2 openal-soft cmake ninja openssl
 ;;
 esac
