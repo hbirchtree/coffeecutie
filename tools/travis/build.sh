0ab154a8d24689886b047829f1df8a65c7c30ea1
Håvard Bjerke <h0wy36@gmail.com>
2016-10-29 03:08:48 +0200

    - Update Travis CI build script, toolchains

--- tools/travis/build.sh
+++ tools/travis/build.sh
@@ -2,10 +2,10 @@
 
 case "${TRAVIS_OS_NAME}" in
 "linux")
-cmake -G"Ninja" -DCMAKE_INSTALL_PREFIX=out
+cmake -G"Ninja" -DCMAKE_TOOLCHAIN_FILE=cmake/Toolchains/native-linux-generic.toolchain.cmake -Ccmake/Preload/x86_64-linux-generic.cmake -DCMAKE_INSTALL_PREFIX=out
 ;;
 "osx")
-cmake -G"Ninja" -DOPENSSL_ROOT_DIR=/usr/local/opt/openssl -DCMAKE_INSTALL_PREFIX=out
+cmake -G"Ninja" -DCMAKE_TOOLCHAIN_FILE=cmake/Toolchains/native-macintosh-generic.toolchain.cmake -Ccmake/Preload/x86_64-osx-generic.cmake -DOPENSSL_ROOT_DIR=/usr/local/opt/openssl -DCMAKE_INSTALL_PREFIX=out
 ;;
 esac
 
