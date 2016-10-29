ce72b82fa5f0f3c279e3641251e5219e5f3f6a65
Håvard Bjerke <h0wy36@gmail.com>
2016-10-29 03:17:35 +0200

    - Add SWIG to Travis CI

--- tools/travis/install-dependencies.sh
+++ tools/travis/install-dependencies.sh
@@ -3,10 +3,11 @@
 case "${TRAVIS_OS_NAME}" in
 "linux")
 sudo apt-get update -qq
-sudo apt-get install -qy libsdl2-dev libopenal-dev cmake ninja-build
+sudo apt-get install -qy libsdl2-dev libopenal-dev cmake ninja-build swig3.0
 ;;
 "osx")
 brew install homebrew/versions/llvm38 --with-clang
-brew install sdl2 openal-soft cmake ninja openssl
+brew install homebrew/versions/swig304
+brew install sdl2 openal-soft cmake ninja openssl swig
 ;;
 esac
