8050578571c16fa07cd27a47b9585ed373c22ad5
Håvard Bjerke <h0wy36@gmail.com>
2016-10-29 02:47:23 +0200

    - Update Travis CI, now Brew Clang 3.8

--- tools/travis/install-dependencies.sh
+++ tools/travis/install-dependencies.sh
@@ -6,6 +6,7 @@ sudo apt-get update -qq
 sudo apt-get install -qy libsdl2-dev libopenal-dev cmake ninja-build
 ;;
 "osx")
+brew install homebrew/versions/llvm38 --with-clang
 brew install sdl2 openal-soft cmake ninja openssl
 ;;
 esac
