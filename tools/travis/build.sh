da9e79e8a9017ea134af12476bba04487508b60f
Håvard Bjerke <h0wy36@gmail.com>
2016-10-29 03:37:51 +0200

    - Really disable Snappy and Flatpak

--- tools/travis/build.sh
+++ tools/travis/build.sh
@@ -2,7 +2,9 @@
 
 case "${TRAVIS_OS_NAME}" in
 "linux")
-cmake -G"Ninja" -DCMAKE_TOOLCHAIN_FILE=cmake/Toolchains/native-linux-generic.toolchain.cmake -Ccmake/Preload/x86_64-linux-generic.cmake -DCMAKE_INSTALL_PREFIX=out -DCOFFEE_GENERATE_FLATPAK=OFF -DCOFFEE_GENERATE_SNAPPY=OFF
+cmake -G"Ninja" -DCMAKE_TOOLCHAIN_FILE=cmake/Toolchains/native-linux-generic.toolchain.cmake -Ccmake/Preload/x86_64-linux-generic.cmake -DCMAKE_INSTALL_PREFIX=out
+
+cmake -DCOFFEE_GENERATE_FLATPAK=OFF -DCOFFEE_GENERATE_SNAPPY=OFF
 ;;
 "osx")
 cmake -G"Ninja" -DCMAKE_TOOLCHAIN_FILE=cmake/Toolchains/native-macintosh-generic.toolchain.cmake -Ccmake/Preload/x86_64-osx-generic.cmake -DOPENSSL_ROOT_DIR=/usr/local/opt/openssl -DSWIG_DIR=/usr/local/opt/swig304 -DCMAKE_INSTALL_PREFIX=out
