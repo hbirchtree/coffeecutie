b086a24c6cec6aeefa075ef6046229d92fc37edf
Håvard Bjerke <h0wy36@gmail.com>
2016-10-29 02:19:43 +0200

    - Update Travis CI dependencies

--- tools/travis/install-dependencies.sh
+++ tools/travis/install-dependencies.sh
@@ -6,6 +6,6 @@ sudo apt-get update -qq
 sudo apt-get install -qy libsdl2-dev libopenal-dev cmake ninja-build
 ;;
 "osx")
-brew install sdl2 openal-soft cmake ninja
+brew install sdl2 openal-soft cmake ninja openssl
 ;;
 esac
