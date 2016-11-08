6c45c61e42f383fae857bcd40f1e222eee81b8c7
Håvard Bjerke <h0wy36@gmail.com>
2016-11-08 03:26:20 +0100

    - Add 0 exit after brew commands in install-dependencies.sh

--- tools/travis/install-dependencies.sh
+++ tools/travis/install-dependencies.sh
@@ -2,12 +2,13 @@
 
 case "${TRAVIS_OS_NAME}" in
 "linux")
-sudo apt-get update -qq
-sudo apt-get install -qy libsdl2-dev libopenal-dev cmake ninja-build swig3.0
+    sudo apt-get update -qq
+    sudo apt-get install -qy libsdl2-dev libopenal-dev cmake ninja-build swig3.0
 ;;
 "osx")
-brew install homebrew/versions/llvm38 --with-clang
-brew install homebrew/versions/swig304
-brew install sdl2 openal-soft cmake ninja openssl
+    brew install homebrew/versions/llvm38 --with-clang
+    brew install homebrew/versions/swig304
+    brew install sdl2 openal-soft cmake ninja openssl
+    exit 0
 ;;
 esac
