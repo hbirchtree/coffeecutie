708382cb46c61b356fe6efc563d0693e02a6226c
Håvard Bjerke <h0wy36@gmail.com>
2016-11-17 20:32:27 +0100

    - Fixes to OSX sysinfo on DeviceName

--- coffee/core/private/plat/environment/osx/sysinfo.cpp
+++ coffee/core/private/plat/environment/osx/sysinfo.cpp
@@ -25,8 +25,12 @@ CString MacSysInfo::GetSystemVersion()
 HWDeviceInfo MacSysInfo::DeviceName()
 {
     static const cstring mod_string = "hw.model";
+    static const cstring rel_string = "kern.osrelease";
+    static const cstring typ_string = "kern.ostype";
 
     CString target;
+    CString kern;
+    CString osrel;
 
     size_t len = 0;
     sysctlbyname(mod_string, nullptr, &len, nullptr, 0);
@@ -35,9 +39,30 @@ HWDeviceInfo MacSysInfo::DeviceName()
     {
         target.resize(len+1);
         sysctlbyname(mod_string, &target[0], &len, nullptr, 0);
+        target.resize(Mem::Search::ChrFind(&target[0], '\0') - &target[0]);
     }
 
-    return HWDeviceInfo("Apple", target, GetSystemVersion());
+    len = 0;
+    sysctlbyname(rel_string, nullptr, &len, nullptr, 0);
+
+    if(len)
+    {
+        osrel.resize(len+1);
+        sysctlbyname(rel_string, &osrel[0], &len, nullptr, 0);
+        osrel.resize(Mem::Search::ChrFind(&osrel[0], '\0') - &osrel[0]);
+    }
+
+    len = 0;
+    sysctlbyname(rel_string, nullptr, &len, nullptr, 0);
+
+    if(len)
+    {
+        kern.resize(len+1);
+        sysctlbyname(rel_string, &kern[0], &len, nullptr, 0);
+        kern.resize(Mem::Search::ChrFind(&kern[0], '\0') - &kern[0]);
+    }
+
+    return HWDeviceInfo("Apple", target, kern + " " + osrel);
 }
 
 }
