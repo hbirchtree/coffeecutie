f244e55dd6138e9f05e7fec2a2ad1857c7193f49
Håvard Bjerke <h0wy36@gmail.com>
2016-12-15 16:40:50 +0100

    - Fix include dir for CoffeeCore

--- cmake/Find/FindStandardLibs.cmake
+++ cmake/Find/FindStandardLibs.cmake
@@ -61,6 +61,9 @@ if(ANDROID)
 
         ${SDL2_LIBRARY}
         )
+    list ( APPEND CORE_INCLUDE_DIR
+        ${CMAKE_SOURCE_DIR}/bindings/android/include
+        )
 #    if("${ANDROID_NATIVE_API_LEVEL}" GREATER 17)
 #        message ( "-- Building with GLES 3.0+ support" )
 #        list ( APPEND CORE_EXTRA_LIBRARIES
