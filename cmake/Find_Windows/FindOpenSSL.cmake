1469492f0ff76329712d8eed6325bcbb2eb6867a
Håvard Bjerke <h0wy36@gmail.com>
2017-01-02 04:03:30 +0100

    - Add new automation commit  - Add Windows-only FindOpenSSL script  - Small fix to generator of FindCoffee script

---
+++ cmake/Find_Windows/FindOpenSSL.cmake
@@ -0,0 +1,43 @@
+set ( OPENSSL_SEARCH_PATHS
+    "C:\\OpenSSL-Win64"
+    "C:\\OpenSSL-Win32"
+    )
+
+find_library ( SSL_CRYPTO_LIBRARY_TMP
+    crypto
+
+    PATH_SUFFIXES
+    lib
+    PATHS
+    ${OPENSSL_SEARCH_PATHS}
+    )
+find_library ( SSL_LIBRARY_TMP
+    ssl
+
+    PATH_SUFFIXES
+    lib
+    PATHS
+    ${OPENSSL_SEARCH_PATHS}
+    )
+
+find_path ( SSL_INCLUDE_DIR_TMP
+    ssl.h
+    ssl2.h
+    ssl3.h
+
+    PATH_SUFFIXES
+    include/openssl
+    include/
+    PATHS
+    ${OPENSSL_SEARCH_PATHS}
+    )
+
+set ( OPENSSL_LIBRARIES "${SSL_LIBRARY_TMP};${SSL_CRYPTO_LIBRARY_TMP}" CACHE STRING "" )
+set ( OPENSSL_INCLUDE_DIR "${SSL_INCLUDE_DIR_TMP}" CACHE PATH "" )
+
+INCLUDE(FindPackageHandleStandardArgs)
+
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(OpenSSL
+    REQUIRED_VARS
+    OPENSSL_LIBRARIES
+    OPENSSL_INCLUDE_DIR)
