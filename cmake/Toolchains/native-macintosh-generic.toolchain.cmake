865ae702ea183f58644c44dd7f0139f6cf2b282a
Håvard Bjerke <h0wy36@gmail.com>
2017-01-10 18:53:16 +0100

    - New LLVM directory structure, thanks Brew

--- cmake/Toolchains/native-macintosh-generic.toolchain.cmake
+++ cmake/Toolchains/native-macintosh-generic.toolchain.cmake
@@ -1,16 +1,12 @@
-# Temporary workaround
+set ( CLANG_PATH "/usr/local/opt/llvm@3.8" )
+set ( CMAKE_C_COMPILER ${CLANG_PATH}/bin/clang-3.8  )
+set ( CMAKE_CXX_COMPILER ${CLANG_PATH}/bin/clang++-3.8 )
 
-if(NOT "$ENV{CXX}" AND NOT "$ENV{CC}")
-    set ( CLANG_PATH "/usr/local/opt/llvm38" )
-    set ( CMAKE_C_COMPILER ${CLANG_PATH}/bin/clang-3.8  )
-    set ( CMAKE_CXX_COMPILER ${CLANG_PATH}/bin/clang++-3.8 )
+include_directories ( "${CLANG_PATH}/lib/llvm-3.8/include/c++/v1" )
+link_directories("${CLANG_PATH}/lib/llvm-3.8/lib")
 
-    include_directories ( "${CLANG_PATH}/lib/llvm-3.8/include/c++/v1" )
-    link_directories("${CLANG_PATH}/lib/llvm-3.8/lib")
-else()
-    set ( CMAKE_C_COMPILER "$ENV{CC}"  )
-    set ( CMAKE_CXX_COMPILER "$ENV{CXX}" )
-endif()
+set ( CMAKE_C_COMPILER "$ENV{CC}"  )
+set ( CMAKE_CXX_COMPILER "$ENV{CXX}" )
 
 # Forcing use of C++11 (or later) libc++
 set ( CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -stdlib=libc++ -std=c++11" CACHE STRING "" )
