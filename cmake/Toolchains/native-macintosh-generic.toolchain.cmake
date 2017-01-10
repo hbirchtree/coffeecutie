6b6e97fca66bbc6d912ca6af08d71495868132a4
Håvard Bjerke <h0wy36@gmail.com>
2017-01-10 18:46:55 +0100

    - Attempt at fixing Travis CI errors

--- cmake/Toolchains/native-macintosh-generic.toolchain.cmake
+++ cmake/Toolchains/native-macintosh-generic.toolchain.cmake
@@ -1,11 +1,17 @@
 # Temporary workaround
-set ( CLANG_PATH "/usr/local/opt/llvm38" )
 
-set ( CMAKE_CXX_COMPILER ${CLANG_PATH}/bin/clang++-3.8 )
-set ( CMAKE_C_COMPILER ${CLANG_PATH}/bin/clang-3.8  )
+if(NOT "$ENV{CXX}" AND NOT "$ENV{CC}")
+    set ( CLANG_PATH "/usr/local/opt/llvm38" )
+    set ( CMAKE_C_COMPILER ${CLANG_PATH}/bin/clang-3.8  )
+    set ( CMAKE_CXX_COMPILER ${CLANG_PATH}/bin/clang++-3.8 )
 
-include_directories ( "${CLANG_PATH}/lib/llvm-3.8/include/c++/v1" )
-link_directories("${CLANG_PATH}/lib/llvm-3.8/lib")
+    include_directories ( "${CLANG_PATH}/lib/llvm-3.8/include/c++/v1" )
+    link_directories("${CLANG_PATH}/lib/llvm-3.8/lib")
+else()
+    set ( CMAKE_C_COMPILER "$ENV{CC}"  )
+    set ( CMAKE_CXX_COMPILER "$ENV{CXX}" )
+endif()
+
 # Forcing use of C++11 (or later) libc++
 set ( CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -stdlib=libc++ -std=c++11" CACHE STRING "" )
 # To prevent undefined symbol errors when creating OSX frameworks
