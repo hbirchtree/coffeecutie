5d495adf2e2ac68c669cfcf5ee354625d4830ed4
Håvard Bjerke <h0wy36@gmail.com>
2017-01-12 14:49:44 +0100

    - Fix oopsie in includes

--- include/coffee/core/plat/plat_file.h
+++ include/coffee/core/plat/plat_file.h
@@ -1,5 +1,6 @@
 #include "file/file_def.h"
 #include "file/android/file.h"
+#include "file/emscripten/file.h"
 #include "file/linux/file.h"
 #include "file/osx/file.h"
 #include "file/windows/file.h"
