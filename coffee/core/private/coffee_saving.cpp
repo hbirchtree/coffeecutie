af540309d31e85fbcc3f3b2fea43402c301cd513
Håvard Bjerke <h0wy36@gmail.com>
2017-01-22 15:27:37 +0100

    - Add initial implementation of IndexedDB storage

--- coffee/core/private/coffee_saving.cpp
+++ coffee/core/private/coffee_saving.cpp
@@ -17,8 +17,41 @@ uint16 AvailableSaveSlots()
     return 1;
 }
 
+#if defined(__EMSCRIPTEN__)
+void emscripten_callback_load(void* arg)
+{
+    int* status = C_FCAST<int*>(arg);
+    cDebug("Loaded file");
+    *status = 1;
+}
+void emscripten_callback_store(void* arg)
+{
+    int* status = C_FCAST<int*>(arg);
+    cDebug("Stored file");
+    *status = 1;
+}
+void emscripten_callback_error(void* arg)
+{
+    int* status = C_FCAST<int*>(arg);
+    cDebug("Failed to store file :(");
+    *status = -1;
+}
+#endif
+
 szptr RestoreMemory(c_ptr data_ptr, szptr *data_size, uint16 slot)
 {
+#if defined(__EMSCRIPTEN__)
+    CString save_file = cStringFormat("Coffee_Data_{0}.dat", slot);
+    int status = 0;
+    emscripten_idb_async_store("Coffee" ,save_file.c_str(), data_ptr, data_size,
+                               &status,
+                               emscripten_callback_store,
+                               emscripten_callback_error);
+
+    while(status == 0);
+
+    return data_size;
+#else
     CString file_str = cStringFormat("CoffeeData.{0}.bin", slot);
     CResources::Resource rsc(file_str.c_str(),
                              ResourceAccess::SpecifyStorage
@@ -40,10 +73,26 @@ szptr RestoreMemory(c_ptr data_ptr, szptr *data_size, uint16 slot)
     MemCpy(data_ptr, rsc.data, rsc.size);
     CResources::FileFree(rsc);
     return rsc.size;
+#endif
 }
 
 szptr SaveMemory(c_cptr data_ptr, szptr data_size, uint16 slot)
 {
+#if defined(__EMSCRIPTEN__)
+    CString save_file = cStringFormat("Coffee_Data_{0}.dat", slot);
+
+    int status = 0;
+
+    emscripten_idb_async_store("Coffee" ,save_file.c_str(), data_ptr, data_size,
+                               &status,
+                               emscripten_callback_store,
+                               emscripten_callback_error);
+
+    /* Await data, we're lazy, maybe do some event handling externally? */
+    while(status == 0);
+
+    return data_size;
+#else
     CString file_str = cStringFormat("CoffeeData.{0}.bin", slot);
     CResources::Resource rsc(file_str.c_str(),
                              ResourceAccess::SpecifyStorage
@@ -59,6 +108,7 @@ szptr SaveMemory(c_cptr data_ptr, szptr data_size, uint16 slot)
         return 0;
 
     return rsc.size;
+#endif
 }
 
 }
