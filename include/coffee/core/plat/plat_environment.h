329d77a687eac8059a3c7e03cf5cdd0c19da9e9e
Håvard Bjerke <h0wy36@gmail.com>
2017-02-04 18:28:36 +0100

    - Add stub for Stacktracer on Emscripten

--- include/coffee/core/plat/plat_environment.h
+++ include/coffee/core/plat/plat_environment.h
@@ -13,6 +13,7 @@
 #include "environment/windows/process.h"
 
 #include "environment/android/stacktrace.h"
+#include "environment/emscripten/stacktrace.h"
 #include "environment/linux/stacktrace.h"
 #include "environment/osx/stacktrace.h"
 #include "environment/windows/stacktrace.h"
