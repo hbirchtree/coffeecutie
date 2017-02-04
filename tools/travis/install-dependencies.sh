7197bccdf314c934f494f72a50ab4b4f95be64b5
Håvard Bjerke <h0wy36@gmail.com>
2017-02-04 23:59:24 +0100

    - Add Unwind as dependency on Travis

--- tools/travis/install-dependencies.sh
+++ tools/travis/install-dependencies.sh
@@ -3,7 +3,7 @@
 case "${TRAVIS_OS_NAME}" in
 "linux")
     sudo apt-get update -qq
-    sudo apt-get install -qy libsdl2-dev libopenal-dev cmake ninja-build swig3.0
+    sudo apt-get install -qy libsdl2-dev libopenal-dev cmake ninja-build swig3.0 libunwind8-dev
 ;;
 "osx")
     brew install homebrew/versions/llvm38 --with-clang
