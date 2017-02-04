0cbeb3a4826de2874ec269cbb03792266e3922c0
Håvard Bjerke <h0wy36@gmail.com>
2017-02-04 17:45:06 +0100

    - Add stacktrace header for Windows, it's a dummy

--- include/coffee/core/plat/plat_environment.h
+++ include/coffee/core/plat/plat_environment.h
@@ -1,25 +1,22 @@
 #include "environment/argument_parse.h"
 
-#include "environment/environment_details.h"
 #include "environment/android/environment.h"
 #include "environment/emscripten/environment.h"
 #include "environment/linux/environment.h"
 #include "environment/osx/environment.h"
 #include "environment/windows/environment.h"
 
-#include "environment/process_def.h"
 #include "environment/android/process.h"
 #include "environment/emscripten/process.h"
 #include "environment/linux/process.h"
 #include "environment/osx/process.h"
 #include "environment/windows/process.h"
 
-#include "environment/stacktrace_def.h"
 #include "environment/android/stacktrace.h"
 #include "environment/linux/stacktrace.h"
 #include "environment/osx/stacktrace.h"
+#include "environment/windows/stacktrace.h"
 
-#include "environment/sysinfo_def.h"
 #include "environment/android/sysinfo.h"
 #include "environment/emscripten/sysinfo.h"
 #include "environment/linux/sysinfo.h"
