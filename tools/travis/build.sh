6d3247ee3304f2d32bdc26a326c174b0e5f0a268
Håvard Bjerke <h0wy36@gmail.com>
2017-02-06 18:28:28 +0100

    - Fix preload/toolchain paths for Travis and Appveyor

--- tools/travis/build.sh
+++ tools/travis/build.sh
@@ -2,12 +2,12 @@
 
 case "${TRAVIS_OS_NAME}" in
 "linux")
-cmake -G"Ninja" -DCMAKE_TOOLCHAIN_FILE=cmake/Toolchains/native-linux-generic.toolchain.cmake -Ccmake/Preload/x86_64-linux-generic.cmake -DCMAKE_INSTALL_PREFIX=out
+cmake -G"Ninja" -DCMAKE_TOOLCHAIN_FILE=cmake/Toolchains/linux-generic_linux.toolchain.cmake -Ccmake/Preload/linux-generic.cmake -DCMAKE_INSTALL_PREFIX=out
 
 cmake -DCOFFEE_GENERATE_FLATPAK=OFF -DCOFFEE_GENERATE_SNAPPY=OFF
 ;;
 "osx")
-cmake -G"Ninja" -DCMAKE_TOOLCHAIN_FILE=cmake/Toolchains/native-macintosh-generic.toolchain.cmake -Ccmake/Preload/x86_64-osx-generic.cmake -DOPENSSL_ROOT_DIR=/usr/local/opt/openssl -DSWIG_DIR=/usr/local/opt/swig304 -DCMAKE_INSTALL_PREFIX=out
+cmake -G"Ninja" -DCMAKE_TOOLCHAIN_FILE=cmake/Toolchains/osx-generic_osx.toolchain.cmake -Ccmake/Preload/osx-generic.cmake -DOPENSSL_ROOT_DIR=/usr/local/opt/openssl -DSWIG_DIR=/usr/local/opt/swig304 -DCMAKE_INSTALL_PREFIX=out
 ;;
 esac
 
