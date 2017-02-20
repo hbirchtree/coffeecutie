df2a9a3e5a5d687afec7ba248c96c4535cfa77dc
Håvard Bjerke <h0wy36@gmail.com>
2017-02-20 18:13:20 +0100

    - Add native-client definition of GetSystemVersion(), you can't really do it

---
+++ coffee/core/private/plat/environment/native-client/sysinfo.cpp
@@ -0,0 +1,14 @@
+#include <coffee/core/plat/environment/emscripten/sysinfo.h>
+
+namespace Coffee{
+namespace Environment{
+namespace Emscripten{
+
+CString EmSysInfo::GetSystemVersion()
+{
+    return "";
+}
+
+}
+}
+}
