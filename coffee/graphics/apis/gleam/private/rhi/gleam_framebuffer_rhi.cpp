99188762efd7f88ca43335577c5b1f1f8d798256
Håvard Bjerke <h0wy36@gmail.com>
2017-05-16 00:37:14 +0200

    - Fix GLES quirk, it's now pretty standard

--- coffee/graphics/apis/gleam/private/rhi/gleam_framebuffer_rhi.cpp
+++ coffee/graphics/apis/gleam/private/rhi/gleam_framebuffer_rhi.cpp
@@ -208,28 +208,14 @@ void GLEAM_RenderTarget::clear(bigscalar depth, int32 stencil)
 
 void GLEAM_RenderTarget::clear(uint32 i, const Vecf4 &color, bigscalar depth)
 {
-    if(GL_CURR_API == GLES_3_0 ||
-       GL_CURR_API == GLES_2_0)
-    {
-        glClearDepthf(C_CAST<scalar>(depth));
-        glClearColor(color.r(), color.g(), color.b(), color.a());
-        glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
-    }else{
-        clear(i,color);
-        clear(depth);
-    }
+    clear(i,color);
+    clear(depth);
 }
 
 void GLEAM_RenderTarget::clear(uint32 i, const Vecf4 &color, bigscalar depth, int32 stencil)
 {
-    if(GL_CURR_API == GLES_3_0 ||
-       GL_CURR_API == GLES_2_0)
-    {
-        glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT|GL_STENCIL_BUFFER_BIT);
-    }else{
-        clear(i,color);
-        clear(depth,stencil);
-    }
+    clear(i,color);
+    clear(depth,stencil);
 }
 
 void GLEAM_RenderTarget::bind(FramebufferT t) const
