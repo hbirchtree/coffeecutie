9f50c5423817f6d0fb0fda084bed89105ecd4e0c
Håvard Bjerke <h0wy36@gmail.com>
2017-06-05 01:01:23 +0200

    - Fixes to Travis/CI dependencies  - Fix Travis/CI standalone builds

--- tools/travis/install-dependencies.sh
+++ tools/travis/install-dependencies.sh
@@ -6,7 +6,6 @@ case "${TRAVIS_OS_NAME}" in
     sudo apt-get install -qy libsdl2-dev libopenal-dev cmake ninja-build swig3.0 libunwind8-dev
 ;;
 "osx")
-    brew install homebrew/versions/llvm38 --with-clang
     brew install homebrew/versions/swig304
     brew install sdl2 openal-soft cmake ninja openssl
     exit 0
