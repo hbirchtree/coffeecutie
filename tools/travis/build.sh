f89953912e0787341561513c1033825bc7948d96
Håvard Bjerke <h0wy36@gmail.com>
2017-06-05 20:33:12 +0200

    - Update Travis/CI script with new CI stuff, hopefully it works..?

--- tools/travis/build.sh
+++ tools/travis/build.sh
@@ -1,16 +1,43 @@
 #!/bin/bash
 
+SOURCE_DIR="$PWD"
+BUILD_DIR="$SOURCE_DIR/multi_build"
+
+CI_DIR="$SOURCE_DIR/tools/makers"
+
+mkdir -p "$BUILD_DIR"
+cd "$BUILD_DIR"
+
+function die()
+{
+    echo " * " $@
+    exit 1
+}
+
+function notify()
+{
+    echo " * " $@
+}
+
+function build_standalone()
+{
+    make -f "$CI_DIR/Makefile.standalone" \
+        -e SOURCE_DIR="$SOURCE_DIR" $@ \
+        -e EXTRA_OPTIONS=""
+
+    # We want to exit if the Make process fails horribly
+    # Should also signify to Travis/CI that something went wrong
+    EXIT_STAT=$?
+    [[ ! "$EXIT_STAT" = 0 ]] && die "Make process failed"
+}
+
 case "${TRAVIS_OS_NAME}" in
 "linux")
-cmake -G"Ninja" -DCMAKE_TOOLCHAIN_FILE=cmake/Toolchains/linux-generic_linux.toolchain.cmake -Ccmake/Preload/linux-generic.cmake -DCMAKE_INSTALL_PREFIX=out
+    build_standalone "$BUILDVARIANT"
 
-cmake -DCOFFEE_GENERATE_FLATPAK=OFF -DCOFFEE_GENERATE_SNAPPY=OFF
+    tar -zcvf "libraries_$BUILDVARIANT.tar.gz" ${BUILD_DIR}/build
 ;;
 "osx")
-cmake -G"Ninja" -DCMAKE_TOOLCHAIN_FILE=cmake/Toolchains/osx-generic_osx.toolchain.cmake -Ccmake/Preload/osx-generic.cmake -DOPENSSL_ROOT_DIR=/usr/local/opt/openssl -DSWIG_DIR=/usr/local/opt/swig304 -DCMAKE_INSTALL_PREFIX=out
+
 ;;
 esac
-
-cmake --build . --target install
-
-tar -zcf binaries_${TRAVIS_OS_NAME}_${CC}.tar.gz out/*
