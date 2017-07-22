2049af3ada0fc1628168f5f23431491042ba32fa
Håvard Bjerke <h0wy36@gmail.com>
2017-07-22 20:31:24 +0200

    - Try to fix Appveyor

--- tools/ci/appveyor-build.ps1
+++ tools/ci/appveyor-build.ps1
@@ -4,4 +4,4 @@ cd $env:BUILD_DIR
 & $env:APPVEYOR_BUILD_FOLDER\$env:MAKEFILE_DIR\Makefile.windows.ps1 -SourceDir $env:APPVEYOR_BUILD_FOLDER -Target $env:BUILDVARIANT -Config Debug -CMakeBin $env:CMAKE_BIN -ExtraArgs ("-DCOFFEE_BUILD_OPENAL=OFF","-DSKIP_HIGHMEM_TESTS=ON","-DSKIP_LINKAGE_TEST=ON")
 
 cd "$env:APPVEYOR_BUILD_FOLDER"
-7z a "$env:ARCHIVEPREFIX"_$env:BUILDVARIANT.zip $env:BUILD_DIR\out
+7z a libraries_$env:BUILDVARIANT.zip $env:BUILD_DIR\out
