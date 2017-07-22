aeabfd6748ba382ae2867b4691b10784a501349a
Håvard Bjerke <h0wy36@gmail.com>
2017-07-22 18:56:26 +0200

    - Maybe fix Appveyor

--- tools/ci/appveyor-build.ps1
+++ tools/ci/appveyor-build.ps1
@@ -1,9 +1,7 @@
 mkdir $env:BUILD_DIR
 cd $env:BUILD_DIR
-$env:MAKEFILE_DIR
-#& $env:APPVEYOR_BUILD_FOLDER\$env:MAKEFILE_DIR\Makefile.windows.ps1 -SourceDir $env:APPVEYOR_BUILD_FOLDER -Target uwp.amd64 -Config Debug -CMakeBin $env:CMAKE_BIN
 
-& $env:APPVEYOR_BUILD_FOLDER\$env:MAKEFILE_DIR\Makefile.windows.ps1 -SourceDir $env:APPVEYOR_BUILD_FOLDER -Target win32.amd64 -Config Debug -CMakeBin $env:CMAKE_BIN -ExtraArgs ("-DCOFFEE_BUILD_OPENAL=OFF","")
+& $env:APPVEYOR_BUILD_FOLDER\$env:MAKEFILE_DIR\Makefile.windows.ps1 -SourceDir $env:APPVEYOR_BUILD_FOLDER -Target $env:BUILDVARIANT -Config Debug -CMakeBin $env:CMAKE_BIN -ExtraArgs ("-DCOFFEE_BUILD_OPENAL=OFF","")
 
 cd "$env:APPVEYOR_BUILD_FOLDER"
-7z a Coffee_Win64_"$env:APPVEYOR_BUILD_NUMBER"_$env:APPVEYOR_REPO_COMMIT.zip $env:INSTALL_DIR
+7z a "$env:ARCHIVEPREFIX"_$env:BUILDVARIANT.zip $env:BUILD_DIR\out
--- tools/ci/appveyor-deps.ps1
+++ tools/ci/appveyor-deps.ps1
@@ -1,15 +1,5 @@
 #git clone -q --recursive --branch=$env:APPVEYOR_REPO_BRANCH https://github.com/$env:APPVEYOR_REPO_NAME.git $env:APPVEYOR_BUILD_FOLDER
 #git checkout -qf %APPVEYOR_REPO_COMMIT%
 
-Invoke-WebRequest https://www.libsdl.org/release/SDL2-devel-2.0.4-VC.zip -OutFile "$env:TEMP/SDL2.zip"
-Expand-Archive -Force -Path "$env:TEMP\SDL2.zip" -DestinationPath "$env:TEMP/SDL2"
-mv -ErrorAction Ignore -Force "$env:TEMP/SDL2/*" "C:\"
-mv -ErrorAction Ignore -Force "C:\SDL2-2.0.4" "C:\SDL2"
-rm -r -ErrorAction Ignore -Force "C:\SDL2\SDL2-2.0.4"
-
 iwr https://chocolatey.org/install.ps1 -UseBasicParsing | iex
 choco install -y cmake
-
-ls C:\
-ls "C:\Program Files"
-ls "C:\Program Files (x86)"
