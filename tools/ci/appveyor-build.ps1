bd7c19293ef075ca263ffca991c07ce0907a39a9
hbirchtree <h0wy36@gmail.com>
2017-08-02 00:01:36 +0200

    - Final fix for Appveyor perhaps?

--- tools/ci/appveyor-build.ps1
+++ tools/ci/appveyor-build.ps1
@@ -5,8 +5,6 @@ cd $env:BUILD_DIR
 
 # "Authorization" = "token $env:GITHUB_TOKEN";
 
-$INTERNAL_BUILD_DIR = "build_$env:BUILDVARIANT"
-
 $LIBRARY_DIR = "$env:BUILD_DIR\libraries"
 
 ForEach($dep in $DEPENDENCIES -split ";") {
@@ -22,13 +20,13 @@ ForEach($dep in $DEPENDENCIES -split ";") {
         ForEach($asset in $assets) {
             if ($asset.name.Contains("$BUILDVARIANT")) {
                 $zipfile = $asset.name
-                $dest_dir = "$INTERNAL_BUILD_DIR\libs"
+                $dest_dir = $LIBRARY_DIR
                 echo "Downloading $dep library..."
                 Invoke-WebRequest $asset.browser_download_url -UseBasicParsing -OutFile $zipfile
                 echo "Successfully downloaded library for $dep!"
 
                 echo "Extracting $dep from archive..."
-                Expand-Archive -Path "$zipfile" -DestinationPath "$dep"
+                Expand-Archive -Path $zipfile -DestinationPath $dest_dir
                 echo "Successfully extracted library $dep!"
             }
         }
@@ -43,9 +41,11 @@ $Args = ("-DCOFFEE_BUILD_OPENSSL=OFF","-DCOFFEE_BUILD_OPENAL=OFF",`
 . $env:APPVEYOR_BUILD_FOLDER\$env:MAKEFILE_DIR\Makefile.windows.ps1 `
     -CMakeBin $env:CMAKE_BIN
 
-BuildProject $env:BUILDVARIANT $env:APPVEYOR_BUILD_FOLDER $LIBRARY_DIR "Debug" $Args
+$BuildDir = "$env:BUILD_DIR\build_$env:BUILDVARIANT"
+
+BuildProject $env:BUILDVARIANT $env:APPVEYOR_BUILD_FOLDER $BuildDir $LIBRARY_DIR "Debug" $Args
 
 cd "$env:APPVEYOR_BUILD_FOLDER"
-7z a libraries_$env:BUILDVARIANT.zip $env:BUILD_DIR\out
+7z a libraries_$env:BUILDVARIANT.zip "$BuildDir\out"
 
 cd $PrevPwd
