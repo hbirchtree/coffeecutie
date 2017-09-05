beff1a86a74755507cd5097ae9cdb2eea48f64c9
Håvard Bjerke <h0wy36@gmail.com>
2017-09-05 18:33:32 +0200

    - Add commit checkout part, very important

--- tools/ci/appveyor-checkout.ps1
+++ tools/ci/appveyor-checkout.ps1
@@ -1,7 +1,10 @@
 $URL = "https://github.com/$env:APPVEYOR_REPO_NAME.git"
 
 if((echo $env:APPVEYOR_PULL_REQUEST_NUMER) != "") {
-    & git clone -q --recursive $URL
+    & git clone -q --recursive $URL $env:SOURCE_DIR
 } else {
-    & git clone -q --recursive --branch=$env:APPVEYOR_REPO_BRANCH $URL
+    & git clone -q --recursive --branch=$env:APPVEYOR_REPO_BRANCH $URL $env:SOURCE_DIR
 }
+
+cd $env:SOURCE_DIR
+git checkout -qf $env:APPVEYOR_REPO_COMMIT
