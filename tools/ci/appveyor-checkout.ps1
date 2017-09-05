7d7e54306d9f3f41bab191ff8c3057ae473b9123
hbirchtree <h0wy36@gmail.com>
2017-09-05 20:37:30 +0200

    - Fix appveyor checkout script

--- tools/ci/appveyor-checkout.ps1
+++ tools/ci/appveyor-checkout.ps1
@@ -1,9 +1,11 @@
 $URL = "https://github.com/$env:APPVEYOR_REPO_NAME.git"
 
-if((echo $env:APPVEYOR_PULL_REQUEST_NUMER) != "") {
-    & git clone -q --recursive $URL $env:SOURCE_DIR
+if($env:APPVEYOR_PULL_REQUEST_NUMBER.Length -Eq 0) {
+    echo " * Doing normal checkout"
+    & git clone -q --recursive --depth=30 --branch=$env:APPVEYOR_REPO_BRANCH $URL $env:SOURCE_DIR
 } else {
-    & git clone -q --recursive --branch=$env:APPVEYOR_REPO_BRANCH $URL $env:SOURCE_DIR
+    echo " * Doing pull request checkout"
+    & git clone -q --recursive --depth=30 $URL $env:SOURCE_DIR
 }
 
 cd $env:SOURCE_DIR
