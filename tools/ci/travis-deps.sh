8009293f8705634151d3f9080b7b678822a17d0f
Håvard Bjerke <h0wy36@gmail.com>
2017-10-11 01:12:04 +0200

    - Try to fix Travis deploys

--- tools/ci/travis-deps.sh
+++ tools/ci/travis-deps.sh
@@ -5,7 +5,7 @@ case "${TRAVIS_OS_NAME}" in
     echo " * Travis/CI people, if you are seeing this, some systems require extended binary format support to work :/"
     docker run --rm --privileged multiarch/qemu-user-static:register
 
-    sudo apt-get -qy install python-requests python-yaml
+    sudo apt-get -qy install python-requests python-yaml python3-requests
 ;;
 "osx")
     #brew install python3
