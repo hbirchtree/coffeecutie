678da8988a09f638fe8080843f23c9be7e0b8d7a
Håvard Bjerke <h0wy36@gmail.com>
2017-11-16 04:16:02 +0100

    - Fix loading of extra symbols on NativeActivity

--- desktop/android/Gradle/app/src/main/java/me/birchtrees/CoffeeNativeActivity.java
+++ desktop/android/Gradle/app/src/main/java/me/birchtrees/CoffeeNativeActivity.java
@@ -1,6 +1,9 @@
 package me.birchtrees;
 
 import android.app.NativeActivity;
+import android.content.ComponentName;
+import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 
@@ -10,9 +13,31 @@ public final class CoffeeNativeActivity extends NativeActivity {
      * https://stackoverflow.com/questions/41059909/hiding-the-navigation-bar-in-pure-android-native
      */
 
+    private String getLibraryName() {
+        try {
+            ActivityInfo info = getPackageManager().getActivityInfo(
+                    new ComponentName(this, this.getClass()),
+                    PackageManager.GET_META_DATA);
+            return info.metaData.getString("android.app.lib_name");
+        }catch (PackageManager.NameNotFoundException e)
+        {
+            System.err.println(e.getMessage());
+        }catch (NullPointerException e)
+        {
+            System.err.println("Failed to get library name");
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+        try {
+            super.onCreate(savedInstanceState);
+        }catch(Exception e)
+        {
+            System.err.println("Fatal error in loading activity, retrying with verbose output");
+            System.loadLibrary(getLibraryName());
+            return;
+        }
         //Hide toolbar
         int SDK_INT = android.os.Build.VERSION.SDK_INT;
         if(SDK_INT >= 19)
@@ -29,6 +54,9 @@ public final class CoffeeNativeActivity extends NativeActivity {
                     });
         }
 
+        System.loadLibrary(getLibraryName());
+
+        smuggleVariable(10, getCacheDir().getAbsolutePath());
     }
 
     protected void onResume() {
@@ -61,4 +89,6 @@ public final class CoffeeNativeActivity extends NativeActivity {
                 | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                 | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
     }
+
+    private native void smuggleVariable(int id, String data);
 }
