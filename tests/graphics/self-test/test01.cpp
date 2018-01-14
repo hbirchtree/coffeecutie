2bbafafeb1d207651ed5046c46a0169bdf16a8f7
Håvard Bjerke <h0wy36@gmail.com>
2018-01-14 14:11:43 +0100

    - Add self-test for graphics testing

---
+++ tests/graphics/self-test/test01.cpp
@@ -0,0 +1,35 @@
+#include <coffee/core/CUnitTesting>
+#include <coffee/core/unit_tests/graphics_framework.h>
+
+#include <coffee/graphics/apis/CGLeamRHI>
+
+using namespace Coffee;
+
+using GFX = RHI::GLEAM::GLEAM_API;
+
+bool framebuffer_read()
+{
+    GFX::DefaultFramebuffer().clear(0, Vecf4(0.0, 1.0, 0.0, 1.0));
+
+    Vector<byte_t> framebuffer;
+    GFX::DumpFramebuffer(GFX::DefaultFramebuffer(),
+                         PixCmp::RGBA,
+                         BitFmt::UByte,
+                         framebuffer);
+
+    CRGBA color = {0, 255, 0, 255};
+
+    if(framebuffer.size() < sizeof(color))
+        return false;
+
+    return MemCmp(framebuffer.data(), &color, sizeof(color));
+}
+
+COFFEE_TEST_SUITE(1) = {
+    {CoffeeTest::GraphicsWrap<framebuffer_read, GFX>,
+        "Framebuffer read-back",
+        "Verify that it is possible to read framebuffer pixels",
+        false, false}
+};
+
+COFFEE_GFX_RUN_TESTS(_tests);
