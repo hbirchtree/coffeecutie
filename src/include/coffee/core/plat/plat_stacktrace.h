e6f9fa62a7f5366173ab883ccb9b787bdd6dc0c7
Håvard Bjerke <h0wy36@gmail.com>
2018-06-10 14:24:56 +0200

    - Fix bad error in gitignore

---
+++ src/include/coffee/core/plat/plat_stacktrace.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include "environment/android/stacktrace.h"
+#include "environment/emscripten/stacktrace.h"
+#include "environment/linux/stacktrace.h"
+#include "environment/osx/stacktrace.h"
+#include "environment/windows/stacktrace.h"
