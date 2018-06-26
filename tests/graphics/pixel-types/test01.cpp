6fdbd8ef33f07417296ab1f3c141ad4927d6ec3c
Håvard Bjerke <h0wy36@gmail.com>
2018-06-26 07:44:40 +0200

    - Fix small unit test error

--- tests/graphics/pixel-types/test01.cpp
+++ tests/graphics/pixel-types/test01.cpp
@@ -1,12 +1,12 @@
 #include <coffee/core/CUnitTesting>
 #include <coffee/core/types/edef/pixenum.h>
+#include <coffee/interfaces/cgraphics_pixops.h>
 
 using namespace Coffee;
 
 struct PixFmt_iterator
 {
-    PixFmt_iterator():
-        pixfmt_counter(C_CAST<u32>(PixFmt::None))
+    PixFmt_iterator() : pixfmt_counter(C_CAST<u32>(PixFmt::None))
     {
     }
 
@@ -34,20 +34,49 @@ struct PixFmt_iterator
 
 bool compressed_format_coverage()
 {
-
+    PixFmt_iterator fmt;
+    while(fmt)
+    {
+        if(IsPixFmtCompressed(fmt))
+        {
+            /* TODO: Do something */
+        }
+        ++fmt;
+    }
 
     return true;
 }
 
 bool regular_format_coverage()
 {
+    PixFmt_iterator fmt;
+
+    while(fmt)
+    {
+        if(!IsPixFmtCompressed(fmt))
+        {
+            auto sampleType = GetPixSampleType(fmt);
+
+            if(sampleType == PixFlg::None)
+                throw implementation_error("no sample type");
+
+            auto component = GetPixComponent(fmt);
+            auto bitFmt    = GetPreferredBitFmt(fmt);
+
+            if(component == PixCmp::None)
+                throw implementation_error("no sample type");
+
+            GetPixSize(bitFmt, component, 16);
+        }
+        ++fmt;
+    }
+
     return true;
 }
 
 COFFEE_TEST_SUITE(2) = {{compressed_format_coverage,
                          "Compressed format coverage",
-                         "Whether all compressed formats are covered by "
-                         "functions relating to their properties"},
+                         "Checking compatibility with functions"},
                         {regular_format_coverage, "Regular format coverage"}};
 
 COFFEE_EXEC_TESTS()
