b8caa5ab8a723f56fc196be349e503a288ede509
Håvard Bjerke <h0wy36@gmail.com>
2018-08-28 20:45:28 +0200

    - Make coverage reports more frequent, every commit

--- toolchain/ci/travis-deploy.sh
+++ toolchain/ci/travis-deploy.sh
@@ -6,13 +6,16 @@ set -x
 source $(dirname $0)/travis-deploy-common.sh
 source $(dirname $0)/travis-build-common.sh
 
+notify "Sending coverage info"
+
+collect_coverage
+
 BINARY_ASSET="$TRAVIS_BUILD_DIR/binaries_${DEPLOYED_BUILDVARIANT}.tar.gz"
 LIBRARY_ASSET="$TRAVIS_BUILD_DIR/libraries_${DEPLOYED_BUILDVARIANT}.tar.gz"
 TARGET_SLUG="$(get_deploy_slug)"
 
 notify "Creating artifacts"
 
-collect_coverage
 
 package_binaries "${BUILD_DIR}" "$BINARY_ASSET"
 package_libraries "${BUILD_DIR}" "$LIBRARY_ASSET"
