22bab7d08145f9ad49ca74b22a38fe90acf4ff28
Håvard Bjerke <h0wy36@gmail.com>
2018-09-09 00:30:06 +0200

    - Fix deployment script

--- toolchain/ci/travis-deploy.sh
+++ toolchain/ci/travis-deploy.sh
@@ -10,16 +10,20 @@ notify "Sending coverage info"
 
 collect_coverage
 
-BINARY_ASSET="$TRAVIS_BUILD_DIR/binaries_${DEPLOYED_BUILDVARIANT}.tar.gz"
-LIBRARY_ASSET="$TRAVIS_BUILD_DIR/libraries_${DEPLOYED_BUILDVARIANT}.tar.gz"
+BINARY_ASSET="binaries_${BUILDVARIANT}.tar.gz"
+LIBRARY_ASSET="libraries_${BUILDVARIANT}.tar.gz"
 TARGET_SLUG="$(get_deploy_slug)"
 
 notify "Creating artifacts"
 
 [ -f ${SOURCE_DIR}/deploy-trigger.sh ] && source ${SOURCE_DIR}/deploy-trigger.sh && exit 0
 
+pushd $TRAVIS_BUILD_DIR
+
 package_binaries "${BUILD_DIR}" "$BINARY_ASSET"
 package_libraries "${BUILD_DIR}" "$LIBRARY_ASSET"
 
 deploy_asset "$BINARY_ASSET" ${TARGET_SLUG}
 deploy_asset "$LIBRARY_ASSET" ${TARGET_SLUG}
+
+popd
