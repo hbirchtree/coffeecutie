36bb4b408162ede40ceb6edcf3e46e94b7243cf7
Håvard Bjerke <h0wy36@gmail.com>
2018-09-09 01:32:46 +0200

    - Fix PressureCooker deployment

--- deploy-post-trigger.sh
+++ deploy-post-trigger.sh
@@ -3,14 +3,11 @@ PRESSURE_ASSET="pressurecooker+${BUILDVARIANT}.tar.gz"
 # Package and release PressureCooker as its own bundle
 
 tar -zcf "${PRESSURE_ASSET}" -C "${BUILD_DIR}" \
-    --exclude=build/*/include \
-    --exclude=build/*/packaged \
-    --exclude=build/*/share \
     $(ls \
-        build/bin/PressureCooker* \
-        build/bin/*/PressureCooker* \
-        build/lib/*/*Pressurize* \
-        build/lib/*Pressurize* \
+        build/*/bin/PressureCooker* \
+        build/*/bin/*/PressureCooker* \
+        build/*/lib/*/*Pressurize* \
+        build/*/lib/*Pressurize* \
     )
 
 package_binaries "${BUILD_DIR}" "${PRESSURE_ASSET}"
