0decc2a053d5445fd0f7ad5dba0fe5813e30baf8
Håvard Bjerke <h0wy36@gmail.com>
2018-09-09 01:50:20 +0200

    - Fix pressure cooker deployment?

--- deploy-post-trigger.sh
+++ deploy-post-trigger.sh
@@ -2,6 +2,11 @@ PRESSURE_ASSET="pressurecooker+${BUILDVARIANT}.tar.gz"
 
 # Package and release PressureCooker as its own bundle
 
+pushd "${BUILD_DIR}"
+
+# Path to PressureCooker and the extensions may vary,
+#  therefore using $(ls ...) to add them conditionally
+# Also, -C does not work with the $(ls ...) workaround, so pushd
 tar -zcf "${PRESSURE_ASSET}" -C "${BUILD_DIR}" \
     $(ls \
         build/*/bin/PressureCooker* \
@@ -10,5 +15,6 @@ tar -zcf "${PRESSURE_ASSET}" -C "${BUILD_DIR}" \
         build/*/lib/*Pressurize* \
     )
 
-package_binaries "${BUILD_DIR}" "${PRESSURE_ASSET}"
+popd
+
 deploy_asset "${PRESSURE_ASSET}" ${TARGET_SLUG}
