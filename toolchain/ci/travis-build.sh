16099de31dcc588439d284da24dc6090916b150b
Håvard Bjerke <h0wy36@gmail.com>
2019-04-07 01:06:52 +0200

    - Add pre-build script support  - Move functions from travis-build-common to travis-common  - Add GitDependencies, for downloading libraries from GitHub  - Remove native-library-bundle checkout from build script

--- toolchain/ci/travis-build.sh
+++ toolchain/ci/travis-build.sh
@@ -8,6 +8,8 @@ notify "Building $BUILDVARIANT"
 
 download_dependencies "${DEPENDENCIES}" || die "Failed to download dependencies"
 
+[ -f ${SOURCE_DIR}/pre-build-script.sh ] && source ${SOURCE_DIR}/pre-build-script.sh
+
 [ -f ${SOURCE_DIR}/build-script.sh ] && source ${SOURCE_DIR}/build-script.sh && exit 0
 
 build_target "${BUILDVARIANT}"
---
+++ toolchain/cmake/Modules/GitDependencies.cmake
@@ -0,0 +1,104 @@
+set ( GIT_DEP_DIR "${CMAKE_BINARY_DIR}/dependencies" CACHE PATH "" )
+set ( GIT_DEP_BUILDVARIANT "" CACHE STRING "" )
+
+function ( DEPENDENCY_GET )
+
+    cmake_parse_arguments ( DEP
+        "REQUIRED"
+        "EXTENSION;SOURCE;TAG;CACHE_LOCATION;LIB_LOCATION"
+        "NAMES"
+
+        ${ARGN}
+        )
+
+    if("${DEP_CACHE_LOCATION}" STREQUAL "")
+        set ( DEP_CACHE_LOCATION "${GIT_DEP_DIR}/cache" )
+    endif()
+
+    if("${DEP_LIB_LOCATION}" STREQUAL "")
+        set ( DEP_LIB_LOCATION "${GIT_DEP_DIR}" )
+    endif()
+
+    if("${DEP_EXTENSION}" STREQUAL "")
+        set ( DEP_EXTENSION "tar.gz" )
+    endif()
+
+    execute_process (
+        COMMAND ${CMAKE_COMMAND} -E make_directory
+            "${DEP_CACHE_LOCATION}"
+        )
+
+    set ( BASE_URL
+        "https://github.com/${DEP_SOURCE}/releases/download/${DEP_TAG}" )
+
+    foreach ( NAME ${DEP_NAMES} )
+        set ( LOCAL_DIR "${DEP_LIB_LOCATION}/${NAME}" )
+
+        #
+        # In order to avoid globbing up the CMAKE_PREFIX_PATH,
+        #  we only append it when it's not there.
+        # We also have to use FORCE in order to make this work properly.
+        # This is probably very bad, but lets FIND_PACKAGE() and pkg-config work
+        #
+        if(NOT "${CMAKE_PREFIX_PATH}" MATCHES "${LOCAL_DIR}" )
+            set (
+                CMAKE_PREFIX_PATH
+                    "${CMAKE_PREFIX_PATH};${LOCAL_DIR}"
+                CACHE STRING ""
+                FORCE
+                PARENT_SCOPE
+                )
+        endif()
+
+        set ( FILE_URL "${BASE_URL}/${NAME}_${GIT_DEP_BUILDVARIANT}.${DEP_EXTENSION}" )
+        set ( LOCAL_FILE "${DEP_CACHE_LOCATION}/${NAME}.${DEP_EXTENSION}" )
+        set ( VERSION_FILE "${LOCAL_FILE}.version" )
+        set ( TARGET_LOCATION "${DEP_LIB_LOCATION}" )
+
+        if( EXISTS "${VERSION_FILE}" )
+            file ( STRINGS "${VERSION_FILE}" LOCAL_VERSION )
+        endif()
+
+        #
+        # Check for pre-existing download version
+        #
+        if ( EXISTS "${LOCAL_FILE}" AND "${LOCAL_VERSION}" STREQUAL "${DEP_TAG}" )
+            message ( STATUS "${NAME} already up to date (${LOCAL_VERSION})" )
+            continue()
+        endif()
+
+        message ( STATUS "Downloading ${NAME} (${DEP_TAG})" )
+
+        file ( DOWNLOAD
+            "${FILE_URL}"
+            "${LOCAL_FILE}"
+            STATUS DL_STAT
+            )
+
+        list ( GET DL_STAT 0 DL_ERROR )
+
+        if(NOT "${DL_ERROR}" STREQUAL "0" AND ${DEP_REQUIRED})
+            message ( STATUS "Error on URL: ${FILE_URL}" )
+            message ( FATAL_ERROR "Failed to download ${NAME} (${DEP_TAG}): ${DL_STAT}" )
+        endif()
+
+        #
+        # Write downloaded version to file
+        #
+        file ( WRITE "${VERSION_FILE}" "${DEP_TAG}" )
+
+        #
+        # Install the library bundle
+        #
+        execute_process (
+            COMMAND ${CMAKE_COMMAND} -E make_directory
+                "${DEP_LIB_LOCATION}/${NAME}"
+            )
+
+        execute_process (
+            COMMAND ${CMAKE_COMMAND} -E tar xf "${LOCAL_FILE}"
+            WORKING_DIRECTORY "${DEP_LIB_LOCATION}/${NAME}"
+            )
+    endforeach()
+
+endfunction()
