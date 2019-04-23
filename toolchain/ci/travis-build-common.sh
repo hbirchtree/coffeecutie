3571e69733888c47e2d378a147999c184f55b124
Håvard Bjerke <h0wy36@gmail.com>
2019-04-23 07:11:37 +0200

    - Gut the old dependency system

--- toolchain/ci/travis-build-common.sh
+++ toolchain/ci/travis-build-common.sh
@@ -24,79 +24,6 @@ function github_api()
     esac
 }
 
-#
-# $1 : configuration list
-# $2 : dependency
-# $3 : version
-#
-function download_libraries()
-{
-    local slug=$(echo $2 | cut -d':' -f1)
-
-    notify "Downloading library ${slug}:${3}"
-    local CONFIG_RELEASE="$(${INFOPY} --source-dir "$SOURCE_DIR" ${1} ${2})"
-    notify "Target version: $CONFIG_RELEASE"
-
-    local ALL_RELEASES=$(github_api list release ${slug} | cut -d'|' -f 3)
-
-    local LATEST_RELEASE=$(${HELPER} get-closest-release "$CONFIG_RELEASE" ${ALL_RELEASES})
-
-    notify "Using release $LATEST_RELEASE for $slug"
-    local CURRENT_ASSET="$(github_api list asset ${slug}:${LATEST_RELEASE} | grep "libraries_${3}.tar.gz" | head -1)"
-    #echo Asset $CURRENT_ASSET
-
-    [[ -z ${CURRENT_ASSET} ]] && echo "Failed to find ${slug}:$3" && return 1
-
-    notify "Found assets: $CURRENT_ASSET (from $LATEST_RELEASE)"
-    local ASSET_ID="$(echo ${CURRENT_ASSET} | cut -d'|' -f 3)"
-    local ASSET_FN="$PWD/$(echo ${CURRENT_ASSET} | cut -d'|' -f 5)"
-
-    github_api pull asset $2 ${ASSET_ID}
-
-    [ ! -f ${ASSET_FN} ] && echo "Failed to pull asset" && return 1
-
-    local PREV_WD="$PWD"
-
-    mkdir -p "$COFFEE_DIR"
-    cd "$COFFEE_DIR"
-
-    tar -xvf "$ASSET_FN" --strip-components=1
-    rm -f "$ASSET_FN"
-
-    cd "$PREV_WD"
-}
-
-function container_run()
-{
-    case "${TRAVIS_OS_NAME}" in
-    "linux")
-        local CONTAINER_DATA=`grep "^$2:" "$CI_DIR/$MAKEFILE" -A 30 | grep 'DOCKER_CONTAINER\|DOCKER_CONFIG'`
-
-        if [ -z `echo ${CONTAINER_DATA} | grep DOCKER_CONFIG` ]; then
-            make -s -f ${CI_DIR}/Makefile.multi custom -e CUSTOM_COMMAND="$1" -e DOCKER_CONFIG=`echo ${CONTAINER_DATA} | cut -d '"' -f 2`
-        else
-            make -s -f ${CI_DIR}/Makefile.multi custom -e CUSTOM_COMMAND="$1" -e DOCKER_CONTAINER=`echo ${CONTAINER_DATA} | cut -d '"' -f 2`
-        fi
-
-    ;;
-    "osx")
-        $@
-    ;;
-    esac
-}
-
-function download_dependencies()
-{
-    #####################################################
-    # First get the dependencies, which we will download
-    #####################################################
-    OLD_IFS=$IFS
-    IFS='%'
-    for dep in $1; do
-        [ "${NODEPS}" != "1" ] && IFS=${OLD_IFS} download_libraries dependencies "$dep" "$BUILDVARIANT" || return 1
-    done
-}
-
 function build_standalone()
 {
     mkdir -p "$SOURCE_DIR" "$COFFEE_DIR" "$BUILD_DIR"
@@ -107,12 +34,6 @@ function build_standalone()
         echo "#### Program versions ###############################"
         echo "#####################################################"
 
-        container_run "cmake --version" "$1"
-        #container_run "clang --version"
-        #container_run "clang++ --version"
-        #container_run "gcc --version"
-        #container_run "g++ --version"
-        #container_run "ld --version"
         tar --version
 
         echo "#####################################################"
@@ -218,12 +139,3 @@ function main()
     [ ! -z ${NODEPLOY} ] && exit 0
 }
 
-#notify "Building $BUILDVARIANT"
-#
-#build_target "${BUILDVARIANT}"
-#
-#if [ -z ${NODEPLOY} ]; then
-#    notify "Creating artifacts"
-#    package_binaries ${BUILD_DIR}
-#    package_libraries ${BUILD_DIR}
-#fi
--- toolchain/ci/travis-build.sh
+++ toolchain/ci/travis-build.sh
@@ -6,8 +6,6 @@ source $(dirname $0)/travis-build-common.sh
 
 notify "Building $BUILDVARIANT"
 
-download_dependencies "${DEPENDENCIES}" || die "Failed to download dependencies"
-
 [ -f ${SOURCE_DIR}/pre-build-script.sh ] && source ${SOURCE_DIR}/pre-build-script.sh
 
 [ -f ${SOURCE_DIR}/build-script.sh ] && source ${SOURCE_DIR}/build-script.sh && exit 0
