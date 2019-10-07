f6ab2dcf95917c9b3c5d1b25ea2204bface9dcaf
Håvard Bjerke <h0wy36@gmail.com>
2019-10-07 03:14:59 +0200

    - Try to refactor AppImage stuff into target properties

--- toolchain/cmake/Packaging/LinuxAppImageBuild.cmake
+++ toolchain/cmake/Packaging/LinuxAppImageBuild.cmake
@@ -23,6 +23,7 @@ function( APPIMAGE_PACKAGE
         DATA
         LIBRARIES LIBRARY_FILES
         ICON_ASSET )
+
     string ( TOLOWER "${APPIMAGE_TITLE}" APPIMAGE_INTERNALNAME )
     string ( MAKE_C_IDENTIFIER "${APPIMAGE_INTERNALNAME}" APPIMAGE_INTERNALNAME )
 
@@ -37,29 +38,43 @@ function( APPIMAGE_PACKAGE
         else()
             set ( APPIMAGE_RUNTIME_BINARY "${RUNTIME_BIN_TMP}" CACHE PATH "" FORCE )
         endif()
-
     endif()
 
     # Some prerequisites
     # TITLE here is used as the name of the final AppImage as well as the desktop entry's name
-    set ( APPIMAGE_TITLE "${APPIMAGE_TITLE}" )
-    set ( APPIMAGE_INTERNALNAME "${APPIMAGE_INTERNALNAME}" )
-    set ( APPIMAGE_LIBRARIES )
-    set ( APPIMAGE_DATA )
 
-    # Icon file to be used for the AppImage, only one in this case, preferrably SVG
-    set ( APPIMAGE_ICON "${ICON_ASSET}" )
-    # We define a way to reference this icon based on where it is located
-    set ( APPIMAGE_ICON_REF "${APPIMAGE_INTERNALNAME}.svg" )
+    set_target_properties ( ${TARGET}
 
-    # This helps the window manager to recognize the program even if it has no embedded or loaded icon
-    set ( APPIMAGE_EXEC_WM ${TARGET} )
+        PROPERTIES
 
-    # Sets the launch variable in .desktop entry
-    set ( APPIMAGE_EXEC ${TARGET} )
+        APPIMAGE_TITLE "${APPIMAGE_TITLE}"
+        APPIMAGE_INTERNALNAME "${APPIMAGE_INTERNALNAME}"
+
+        # Icon file to be used for the AppImage, only one in this case, preferrably SVG
+        # This helps the window manager to recognize the program even if it has no embedded or loaded icon
+        APPIMAGE_ICON "${ICON_ASSET}"
+
+        # We define a way to reference this icon based on where it is located
+        APPIMAGE_ICON_REF "$<TARGET_PROPERTY:APPIMAGE_INTERNALNAME>.svg"
+
+        # Sets the launch variable in .desktop entry
+        APPIMAGE_EXEC_WM "${TARGET}"
+
+        APPIMAGE_LIBRARIES "${LIBRARIES}"
+        APPIMAGE_LIBRARY_FILES "${LIBRARY_FILES}"
+        APPIMAGE_DATA "${DATA}"
+
+        APPIMAGE_FINAL_NAME
+            "${APPIMAGE_OUTPUT_DIRECTORY}/${TARGET}.AppImage"
+
+        APPIMAGE_ASSET_DIR   assets
+        APPIMAGE_BINARY_DIR  bin
+        APPIMAGE_LIBRARY_DIR lib
+        )
 
     # This directory is used for temporary files, might get messy
-    set ( APPIMAGE_CACHE_DIR "${APPIMAGE_WORKING_DIRECTORY}/${APPIMAGE_INTERNALNAME}_cache" )
+    set ( APPIMAGE_CACHE_DIR
+        "${APPIMAGE_WORKING_DIRECTORY}/${APPIMAGE_INTERNALNAME}_cache" )
     # Where the AppDir is generated
     set ( APPIMAGE_INTERMEDIATE_DIR
         "${APPIMAGE_WORKING_DIRECTORY}/${APPIMAGE_INTERNALNAME}.AppDir" )
@@ -78,72 +93,88 @@ function( APPIMAGE_PACKAGE
         set ( APPIMAGE_ASSET_DIR "${APPIMAGE_INTERMEDIATE_DIR}/usr/share" )
         set ( APPIMAGE_LIBRARY_DIR "${APPIMAGE_INTERMEDIATE_DIR}/usr/lib" )
     endif()
-    set ( APPIMAGE_FINAL_NAME "${APPIMAGE_OUTPUT_DIRECTORY}/${APPIMAGE_TITLE}.AppImage" )
-
-    list ( APPEND APPIMAGE_LIBRARIES
-        ${LIBRARIES} )
-    list ( APPEND APPIMAGE_DATA
-        ${DATA} )
 
     add_custom_command ( TARGET ${TARGET}
         PRE_BUILD
 
         # Remove the previous AppImage file to avoid confusion when generating a new one
-        COMMAND ${CMAKE_COMMAND} -E remove "${APPIMAGE_FINAL_NAME}"
+        COMMAND ${CMAKE_COMMAND} -E remove
+            "$<TARGET_PROPERTY:${TARGET},APPIMAGE_FINAL_NAME>"
 
         # Create some necessary directory structure in AppDir
-        COMMAND ${CMAKE_COMMAND} -E make_directory "${APPIMAGE_OUTPUT_DIRECTORY}"
-        COMMAND ${CMAKE_COMMAND} -E make_directory "${APPIMAGE_BINARY_DIR}"
-        COMMAND ${CMAKE_COMMAND} -E make_directory "${APPIMAGE_CACHE_DIR}"
-        COMMAND ${CMAKE_COMMAND} -E make_directory "${APPIMAGE_LIBRARY_DIR}"
-        COMMAND ${CMAKE_COMMAND} -E make_directory "${APPIMAGE_ASSET_DIR}"
+        COMMAND ${CMAKE_COMMAND} -E make_directory
+            "${APPIMAGE_INTERMEDIATE_DIR}/$<TARGET_PROPERTY:${TARGET},APPIMAGE_OUTPUT_DIRECTORY>"
+        COMMAND ${CMAKE_COMMAND} -E make_directory
+            "${APPIMAGE_INTERMEDIATE_DIR}/$<TARGET_PROPERTY:${TARGET},APPIMAGE_BINARY_DIR>"
+        COMMAND ${CMAKE_COMMAND} -E make_directory
+            "${APPIMAGE_INTERMEDIATE_DIR}/$<TARGET_PROPERTY:${TARGET},APPIMAGE_CACHE_DIR>"
+        COMMAND ${CMAKE_COMMAND} -E make_directory
+            "${APPIMAGE_INTERMEDIATE_DIR}/$<TARGET_PROPERTY:${TARGET},APPIMAGE_LIBRARY_DIR>"
+        COMMAND ${CMAKE_COMMAND} -E make_directory
+            "${APPIMAGE_INTERMEDIATE_DIR}/$<TARGET_PROPERTY:${TARGET},APPIMAGE_ASSET_DIR>"
         )
 
     # Copy and configure some data for the AppDir
-    configure_file (
-        "${APPIMAGE_ICON}"
-        "${APPIMAGE_ICON_TARGET}"
-        COPYONLY
+#    configure_file (
+#        "${APPIMAGE_ICON}"
+#        "${APPIMAGE_ICON_TARGET}"
+#        COPYONLY
+#        )
+#    configure_file (
+#        "${APPIMAGE_ICON}"
+#        "${APPIMAGE_INTERMEDIATE_DIR}/.DirIcon"
+#        COPYONLY
+#        )
+#    configure_file (
+#        "${APPIMAGE_CONFIG_DIR}/set_icon.py"
+#        "${APPIMAGE_INTERMEDIATE_DIR}/set_icon.py"
+#        COPYONLY
+#        )
+#    configure_file (
+#        "${APPIMAGE_CONFIG_DIR}/application.desktop.in"
+#        "${APPIMAGE_INTERMEDIATE_DIR}/${APPIMAGE_INTERNALNAME}.desktop"
+#        @ONLY
+#        )
+    # Build-time step that places libraries, data and scripts into AppDir
+    add_custom_command ( TARGET ${TARGET}
+        POST_BUILD
+        COMMAND ${CMAKE_COMMAND}
+            -DCMAKE_COMMAND=${CMAKE_COMMAND}
+            -DTARGET=${TARGET}
+            -DAPPDIR=${APPIMAGE_INTERMEDIATE_DIR}
+            -DTITLE=$<TARGET_PROPERTY:${TARGET},APPIMAGE_TITLE>
+
+            -DLIBRARY_DIR=${APPIMAGE_INTERMEDIATE_DIR}/$<TARGET_PROPERTY:${TARGET},APPIMAGE_LIBRARY_DIR>
+            -DASSET_DIR=${APPIMAGE_INTERMEDIATE_DIR}/$<TARGET_PROPERTY:${TARGET},APPIMAGE_ASSET_DIR>
+
+            -DDATA=$<TARGET_PROPERTY:${TARGET},APPIMAGE_DATA>
+            -P ${APPIMAGE_CONFIG_DIR}/Assemble.cmake.in
         )
-    configure_file (
-        "${APPIMAGE_ICON}"
-        "${APPIMAGE_INTERMEDIATE_DIR}/.DirIcon"
-        COPYONLY
-        )
-    configure_file (
-        "${APPIMAGE_CONFIG_DIR}/set_icon.py"
-        "${APPIMAGE_INTERMEDIATE_DIR}/set_icon.py"
-        COPYONLY
-        )
-    configure_file (
-        "${APPIMAGE_CONFIG_DIR}/application.desktop.in"
-        "${APPIMAGE_INTERMEDIATE_DIR}/${APPIMAGE_INTERNALNAME}.desktop"
-        @ONLY
-        )
-    if(NOT APPIMAGE_FOLLOW_STANDARD)
-        file (
-            COPY "${APPIMAGE_CONFIG_DIR}/startup.sh.in"
-            DESTINATION "${APPIMAGE_CACHE_DIR}"
-            FILE_PERMISSIONS OWNER_READ OWNER_WRITE OWNER_EXECUTE
-            )
-        configure_file (
-            "${APPIMAGE_CACHE_DIR}/startup.sh.in"
-            "${APPIMAGE_INTERMEDIATE_DIR}/AppRun"
-            @ONLY
-            )
-    else()
-        configure_file (
-            "${APPIMAGE_APPRUN_PROGRAM}"
-            "${APPIMAGE_INTERMEDIATE_DIR}/AppRun"
-            COPYONLY
-            )
-    endif()
+
+#    if(NOT APPIMAGE_FOLLOW_STANDARD)
+#        file (
+#            COPY "${APPIMAGE_CONFIG_DIR}/startup.sh.in"
+#            DESTINATION "${APPIMAGE_CACHE_DIR}"
+#            FILE_PERMISSIONS OWNER_READ OWNER_WRITE OWNER_EXECUTE
+#            )
+#        configure_file (
+#            "${APPIMAGE_CACHE_DIR}/startup.sh.in"
+#            "${APPIMAGE_INTERMEDIATE_DIR}/AppRun"
+#            @ONLY
+#            )
+#    else()
+#        configure_file (
+#            "${APPIMAGE_APPRUN_PROGRAM}"
+#            "${APPIMAGE_INTERMEDIATE_DIR}/AppRun"
+#            COPYONLY
+#            )
+#    endif()
 
     # Copy resources into AppDir
     foreach ( RESC ${DATA} )
         add_custom_command ( TARGET ${TARGET}
             PRE_BUILD
-            COMMAND ${CMAKE_COMMAND} -E copy_directory "${RESC}" "${APPIMAGE_ASSET_DIR}"
+            COMMAND ${CMAKE_COMMAND} -E copy_directory "${RESC}" "${APPIMAGE_INTERMEDIATE_DIR}/$<TARGET_PROPERTY:${TARGET},APPIMAGE_ASSET_DIR>"
             )
     endforeach()
 
@@ -151,24 +182,16 @@ function( APPIMAGE_PACKAGE
     foreach ( LIB ${LIBRARY_FILES} )
         add_custom_command ( TARGET ${TARGET}
             POST_BUILD
-            COMMAND ${CMAKE_COMMAND} -E copy "${LIB}" "${APPIMAGE_LIBRARY_DIR}"
+            COMMAND ${CMAKE_COMMAND} -E copy "${LIB}" "${APPIMAGE_INTERMEDIATE_DIR}/$<TARGET_PROPERTY:${TARGET},APPIMAGE_LIBRARY_DIR>"
             )
     endforeach()
 
     foreach ( LIB ${LIBRARIES} )
         add_custom_command ( TARGET ${TARGET}
             POST_BUILD
-            COMMAND ${CMAKE_COMMAND} -E copy "$<TARGET_FILE:${LIB}>" "${APPIMAGE_LIBRARY_DIR}"
+            COMMAND ${CMAKE_COMMAND} -E copy "$<TARGET_FILE:${LIB}>" "${APPIMAGE_INTERMEDIATE_DIR}/$<TARGET_PROPERTY:${TARGET},APPIMAGE_LIBRARY_DIR>"
             )
     endforeach()
-
-    if("${CMAKE_BUILD_TYPE}" STREQUAL "Release" AND
-            (NOT "${LIBRARIES}" STREQUAL "" OR NOT "${LIBRARY_FILES}" STREQUAL ""))
-        add_custom_command ( TARGET ${TARGET}
-            POST_BUILD
-            COMMAND bash -c '${CMAKE_STRIP} `find ${APPIMAGE_LIBRARY_DIR} -type f`'
-            )
-    endif()
 
     # Copy the binary to AppDir
     add_custom_command ( TARGET ${TARGET}
@@ -193,16 +216,18 @@ function( APPIMAGE_PACKAGE
                 "${APPIMAGE_INTERMEDIATE_SQUASH}"
                 -root-owned -noappend
         # Concatenate AppImage runtime with image
-        COMMAND cat "${APPIMAGE_RUNTIME_BINARY}" >> "${APPIMAGE_FINAL_NAME}"
-        COMMAND cat "${APPIMAGE_INTERMEDIATE_SQUASH}" >> "${APPIMAGE_FINAL_NAME}"
+        COMMAND cat "${APPIMAGE_RUNTIME_BINARY}" >>
+                        "$<TARGET_PROPERTY:${TARGET},APPIMAGE_FINAL_NAME>"
+        COMMAND cat "${APPIMAGE_INTERMEDIATE_SQUASH}" >>
+                        "$<TARGET_PROPERTY:${TARGET},APPIMAGE_FINAL_NAME>"
 
         # Make image executable
-        COMMAND chmod +x "${APPIMAGE_FINAL_NAME}"
+        COMMAND chmod +x "$<TARGET_PROPERTY:${TARGET},APPIMAGE_FINAL_NAME>"
         )
 
     install (
         FILES
-        "${APPIMAGE_FINAL_NAME}"
+        "$<TARGET_PROPERTY:${TARGET},APPIMAGE_FINAL_NAME>"
 
         DESTINATION
         "${CMAKE_PACKAGED_OUTPUT_PREFIX}/linux-appimage"
