7247e71dfba7cbff585ba97da4666dc2b46f29b3
Håvard Bjerke <h0wy36@gmail.com>
2019-10-22 23:26:42 +0200

    - Minor adjustment to Linux for Azure

--- toolchain/ci/travis-deps.sh
+++ toolchain/ci/travis-deps.sh
@@ -8,7 +8,7 @@ case "${TRAVIS_OS_NAME}" in
     echo " * Travis/CI people, if you are seeing this, some systems require extended binary format support to work :/"
     docker run --rm --privileged multiarch/qemu-user-static:register
 
-    sudo apt-get -qy install python3-pip python-requests python-yaml python3-requests python3-paho-mqtt
+    sudo apt-get -qy install python3-pip python-requests python-yaml python3-requests python3-paho-mqtt python3-setuptools python3-dev
 ;;
 "osx")
     #brew install sdl2 cmake openssl python3 python jq
