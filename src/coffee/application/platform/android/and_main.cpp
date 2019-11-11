e3c65d93509af12fd736ac131e6b7e5d1ffeb869
Håvard Bjerke <h0wy36@gmail.com>
2019-11-11 23:11:50 +0100

    - Remove unused CMake files  - Fixes for Android structure  - Simplify handling of Assimp

--- src/coffee/application/platform/android/and_main.cpp
+++ src/coffee/application/platform/android/and_main.cpp
@@ -716,33 +716,6 @@ STATICINLINE void StartEventProcessing()
  *
  */
 
-namespace jnipp {
-
-thread_local android::ScopedJNI* jniScope = nullptr;
-
-android::ScopedJNI* SwapJNI(android::ScopedJNI* jniScope)
-{
-    auto prevScope  = jniScope;
-    jnipp::jniScope = jniScope;
-
-    return prevScope;
-}
-
-JNIEnv* GetJNI()
-{
-    if(!jniScope)
-        throw undefined_behavior("no JNI environment");
-
-    return jniScope->env();
-}
-
-JavaVM* GetVM()
-{
-    return Coffee::coffee_app->activity->vm;
-}
-
-} // namespace jnipp
-
 namespace android {
 
 using namespace Coffee;
