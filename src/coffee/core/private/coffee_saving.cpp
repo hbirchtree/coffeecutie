aabf39d7db99e74ce6473453b5932e965a0c1c96
Håvard Bjerke <h0wy36@gmail.com>
2019-12-10 12:38:30 +0100

    - Fix some minor build errors

--- src/coffee/core/private/coffee_saving.cpp
+++ src/coffee/core/private/coffee_saving.cpp
@@ -55,7 +55,8 @@ void emscripten_callback_load(void* arg, void* data, int size)
     DataStatus* status = C_FCAST<DataStatus*>(arg);
     cDebug("Loaded file");
     if(size <= C_CAST<int>(*status->size))
-        MemCpy(Bytes::From(data, size), Bytes::From(status->ptr, size));
+        MemCpy(
+            Bytes::FromBytes(data, size), Bytes::FromBytes(status->ptr, size));
     status->status = 1;
 }
 void emscripten_callback_store(void* arg)
