4f9ffc8cfb7e7947771fda6005f937768fe38782
Håvard Bjerke <h0wy36@gmail.com>
2023-04-27 23:03:22 +0200

    vcpkg: Fix config

--- toolchain/vcpkg/ports/bcdec/FindBCDec.cmake
+++
@@ -1,9 +0,0 @@
-include(${CMAKE_ROOT}/Modules/FindPackageHandleStandardArgs.cmake)
-include(${CMAKE_ROOT}/Modules/SelectLibraryConfigurations.cmake)
-
-if(NOT BCDec_INCLUDE_DIR)
-    find_path(BCDec_INCLUDE_DIR NAMES bcdec.h PATHS ${BCDec_DIR} PATH_SUFFIXES include)
-endif()
-
-find_package_handle_standard_args(BCDec DEFAULT_MSG BCDec_INCLUDE_DIR)
-mark_as_advanced(BCDec_INCLUDE_DIR)
