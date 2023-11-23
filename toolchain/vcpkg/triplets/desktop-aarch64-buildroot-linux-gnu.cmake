137318a35d1b6428115b06e1b2d60a64fbe0af8d
Håvard Bjerke <h0wy36@gmail.com>
2023-11-23 03:28:35 +0100

    toolchain: Appimage, aarch64 tweaks

--- toolchain/vcpkg/triplets/desktop-aarch64-buildroot-linux-gnu.cmake
+++ toolchain/vcpkg/triplets/desktop-aarch64-buildroot-linux-gnu.cmake
@@ -5,7 +5,7 @@ set(VCPKG_LIBRARY_LINKAGE static)
 set(VCPKG_CMAKE_SYSTEM_NAME Linux)
 set(VCPKG_CMAKE_SYSTEM_VERSION 1)
 
-set(VCPKG_DEP_INFO_OVERRIDE_VARS linux;arm;arm64;armv8;aarch64;wayland)
+set(VCPKG_DEP_INFO_OVERRIDE_VARS linux;arm;arm64;armv8;aarch64;wayland;x11)
 set(TOOLCHAIN_PREFIX aarch64-buildroot-linux-gnu)
 set(VCPKG_OVERLAY_PORTS ${CMAKE_CURRENT_LIST_DIR}/../ports)
 set(VCPKG_CHAINLOAD_TOOLCHAIN_FILE ${CMAKE_CURRENT_LIST_DIR}/../../cmake/Toolchains/aarch64-buildroot-linux-gnu.toolchain.cmake)
