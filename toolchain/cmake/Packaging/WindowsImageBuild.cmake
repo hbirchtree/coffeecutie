c6c3b78aa7701e4279ca090cc6bee01f7e780b34
Håvard Bjerke <h0wy36@gmail.com>
2024-03-18 22:02:28 +0100

    toolchain: Remove references to UWP
    It was a mistake

--- toolchain/cmake/Packaging/WindowsImageBuild.cmake
+++ toolchain/cmake/Packaging/WindowsImageBuild.cmake
@@ -82,26 +82,21 @@ macro(
       string(TOLOWER "${file_name}" file_name_lower)
 
       if(NOT ("${file_name_lower}" STREQUAL "thumbs.db"))
-        if(NOT WIN_UWP)
-          # On Win32, package it into the .exe file
-
-          # If there is a directory path, append a "_" for it to be correct This
-          # is disgusting.
-          if(file_dir)
-            set(file_dir "${file_dir}/")
-          endif()
-          # Set virtual filename
-          set(virt_fname "${file_dir}${file_name}")
-          string(REPLACE "_" "___" virt_fname "${virt_fname}")
-          string(REPLACE "/" "_" virt_fname "${virt_fname}")
-          string(REPLACE "\\" "_" virt_fname "${virt_fname}")
-          # Insert the file with directory path and filename into the .rc file
-          file(APPEND "${RESOURCE_DESCRIPTOR}"
-               "\"${virt_fname}\" CF_RES \"${file_full}\" ${LINESHIFT}"
-          )
-        else()
-          list(APPEND RESOURCE_FILES "${file_full}")
+        # On Win32, package it into the .exe fil
+        # If there is a directory path, append a "_" for it to be correct This
+        # is disgusting.
+        if(file_dir)
+          set(file_dir "${file_dir}/")
         endif()
+        # Set virtual filename
+        set(virt_fname "${file_dir}${file_name}")
+        string(REPLACE "_" "___" virt_fname "${virt_fname}")
+        string(REPLACE "/" "_" virt_fname "${virt_fname}")
+        string(REPLACE "\\" "_" virt_fname "${virt_fname}")
+        # Insert the file with directory path and filename into the .rc file
+        file(APPEND "${RESOURCE_DESCRIPTOR}"
+             "\"${virt_fname}\" CF_RES \"${file_full}\" ${LINESHIFT}"
+        )
       endif()
       # Increment resource number, inserted into .rc file
       math(EXPR RESC_NUM "${RESC_NUM} + 1")
@@ -123,12 +118,6 @@ macro(
   )
 
   configure_file("${WINDOWS_DESKTOP_DIR}/icon.ico" "icon.ico" COPYONLY)
-
-  # Finally we stir the smelly gak into PE
-
-  if(MSVC AND WIN_UWP)
-    set(OPTIONS WIN32)
-  endif()
 
   if(RESC_NUM STREQUAL "0")
     set(RESOURCE_DESCRIPTOR)
@@ -157,109 +146,9 @@ macro(
     # ${RESOURCE_FILES}
   )
 
-  if(WIN_UWP)
-    target_include_directories(
-      ${TARGET} PRIVATE $<BUILD_INTERFACE:${SDL2_INCLUDE_DIR}>
-    )
-    target_include_directories(
-      ${TARGET} PRIVATE $<BUILD_INTERFACE:${ANGLE_INCLUDE_DIR}>
-    )
-
-    # I need these seeds for mye research, Morty, gotta stuff it waaay up there,
-    # Morty.
-    target_link_libraries(${TARGET} PUBLIC ${SDL2_LIBRARY})
-    set_source_files_properties(
-      ${SDL2_MAIN_C_FILE} PROPERTIES COMPILE_FLAGS /ZW
-    )
-    # set_target_properties ( ${TARGET} PROPERTIES RESOURCE
-    # "${SDL2_LIBRARY_BIN};${ANGLE_LIBRARIES_BIN}" )
-
-    execute_process(
-      COMMAND cmake -E make_directory ${CMAKE_CURRENT_BINARY_DIR}/${TARGET}.dir
-    )
-
-    add_custom_command(
-      TARGET ${TARGET}
-      PRE_LINK
-      COMMAND
-        ${CMAKE_COMMAND} -E copy_if_different
-        ${COFFEE_DESKTOP_DIRECTORY}/windows/icon_150.png
-        ${CMAKE_CURRENT_BINARY_DIR}/${TARGET}.dir/Logo.png
-    )
-    add_custom_command(
-      TARGET ${TARGET}
-      PRE_LINK
-      COMMAND
-        ${CMAKE_COMMAND} -E copy_if_different
-        ${COFFEE_DESKTOP_DIRECTORY}/windows/icon_44.png
-        ${CMAKE_CURRENT_BINARY_DIR}/${TARGET}.dir/SmallLogo.png
-    )
-    add_custom_command(
-      TARGET ${TARGET}
-      PRE_LINK
-      COMMAND
-        ${CMAKE_COMMAND} -E copy_if_different
-        ${COFFEE_DESKTOP_DIRECTORY}/windows/icon_44.png
-        ${CMAKE_CURRENT_BINARY_DIR}/${TARGET}.dir/SmallLogo44x44.png
-    )
-    add_custom_command(
-      TARGET ${TARGET}
-      PRE_LINK
-      COMMAND
-        ${CMAKE_COMMAND} -E copy_if_different
-        ${COFFEE_DESKTOP_DIRECTORY}/windows/icon_50.png
-        ${CMAKE_CURRENT_BINARY_DIR}/${TARGET}.dir/StoreLogo.png
-    )
-
-    add_custom_command(
-      TARGET ${TARGET}
-      PRE_LINK
-      COMMAND
-        ${CMAKE_COMMAND} -E copy_if_different
-        ${COFFEE_DESKTOP_DIRECTORY}/windows/banner_620.png
-        ${CMAKE_CURRENT_BINARY_DIR}/${TARGET}.dir/SplashScreen.png
-    )
-
-    foreach(durr ${RESOURCES})
-      file(GLOB_RECURSE TMP ${durr}/*)
-      foreach(file_full ${TMP})
-        # First, get a relative filename This is used to describe structure
-        file(RELATIVE_PATH file_dir ${durr} ${file_full})
-        get_filename_component(file_dir "${file_dir}" DIRECTORY)
-        get_filename_component(file_name "${file_full}" NAME)
-        # We get a lower-case version to compare with other filenames
-        string(TOLOWER "${file_name}" file_name_lower)
-
-        if(NOT file_dir)
-          set(file_dir ".")
-        endif()
-
-        if(NOT ("${file_name_lower}" STREQUAL "thumbs.db"))
-          # On UWP, mark it as a .appx content file
-          set_source_files_properties(
-            "${file_full}" PROPERTIES VS_DEPLOYMENT_CONTENT 1
-                                      VS_DEPLOYMENT_LOCATION "${file_dir}"
-          )
-        endif()
-      endforeach()
-    endforeach()
-
-    foreach(var ${INCLUDED_LIBS})
-      set_source_files_properties(
-        "${var}" PROPERTIES VS_DEPLOYMENT_CONTENT 1 VS_DEPLOYMENT_LOCATION "."
-      )
-    endforeach()
-  endif()
-
   set_target_properties(
     ${TARGET} PROPERTIES VERSION ${COFFEE_BUILD_STRING} SOVERSION 1
   )
-  if(NOT WIN_UWP)
-    install(TARGETS ${TARGET} DESTINATION bin)
-    install(FILES ${BUNDLE_LIBS} DESTINATION bin)
-  else()
-    install(DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/AppPackages/${TARGET}
-            DESTINATION "${CMAKE_PACKAGED_OUTPUT_PREFIX}/windows-universal"
-    )
-  endif()
+  install(TARGETS ${TARGET} DESTINATION bin)
+  install(FILES ${BUNDLE_LIBS} DESTINATION bin)
 endmacro()
