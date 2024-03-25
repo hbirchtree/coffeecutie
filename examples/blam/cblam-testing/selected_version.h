f3d93c5ff975b5aafe3afe8bf1eb4245ac11f714
Håvard Bjerke <h0wy36@gmail.com>
2024-03-25 21:52:23 +0100

    github: Build release, make Blam version configurable

--- examples/blam/cblam-testing/selected_version.h
+++ examples/blam/cblam-testing/selected_version.h
@@ -2,5 +2,9 @@
 
 #include <blam/volta/blam_versions.h>
 
-using halo_version                    = blam::pc_version_t;
+#ifndef BLAM_HALO_VERSION
+#define BLAM_HALO_VERSION pc_version_t
+#endif
+
+using halo_version                    = blam::BLAM_HALO_VERSION;
 constexpr halo_version halo_version_v = halo_version();
