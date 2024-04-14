9deef31807c8ca34e1786933030a34f8e09429ca
Håvard Bjerke <h0wy36@gmail.com>
2024-04-14 04:27:57 +0200

    toolchain: Fix ARM32, Emscripten shared linkage

--- toolchain/cmake/Toolchains/arm-buildroot-linux-gnueabihf.toolchain.cmake
+++ toolchain/cmake/Toolchains/arm-buildroot-linux-gnueabihf.toolchain.cmake
@@ -6,8 +6,6 @@ include(${CMAKE_CURRENT_LIST_DIR}/common/toolchain-prefix.cmake)
 set(CMAKE_C_COMPILER "${TOOLCHAIN_ROOT}/bin/${TOOLCHAIN_TOOL}gcc")
 set(CMAKE_CXX_COMPILER "${TOOLCHAIN_ROOT}/bin/${TOOLCHAIN_TOOL}g++")
 
-# add_definitions ( -D__BEAGLEBONE__=1 -D__BEAGLEBONEBLACK__=1 )
-
 add_compile_options(-mtune=cortex-a8 -march=armv7-a+simd+vfpv3 -mfloat-abi=hard)
 add_link_options(
   -mtune=cortex-a8 -march=armv7-a+simd+vfpv3 -mfloat-abi=hard
