db84f4a3fcf2dafea46ca54a4410a45537dd4018
Håvard Bjerke <h0wy36@gmail.com>
2024-08-04 22:10:54 +0200

    platform: Fix signature of Android's ::write

--- src/peripheral/platform/include/platforms/android/rdwrio.h
+++ src/peripheral/platform/include/platforms/android/rdwrio.h
@@ -26,9 +26,9 @@ FORCEDINLINE result<mem_chunk<char>, posix::posix_error> read(
 }
 
 FORCEDINLINE std::optional<posix::posix_error> write(
-    detail::android_fd_t const&  file,
-    mem_chunk<const char> const& data,
-    write_params_t const&        params = {})
+    detail::android_fd_t const&       file,
+    semantic::Span<const char> const& data,
+    write_params_t const&             params = {})
 {
     if(detail::is_asset(file))
         return posix::posix_error{EACCES};
