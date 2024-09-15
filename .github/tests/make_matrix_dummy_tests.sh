9641e01d69b902bea1407af5eb2a23c86ce4761d
Håvard Bjerke <h0wy36@gmail.com>
2024-09-15 20:08:15 +0200

    github: Split software render tests into matrix jobs

---
+++ .github/tests/make_matrix_dummy_tests.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+GL_CONFIGS=$(jq -c .graphics.gl[] dummy_plug.json)
+ES_CONFIGS=$(jq -c .graphics.gles[] dummy_plug.json)
+
+for CFG in $GL_CONFIGS; do
+    MAJOR=$(jq .major <<< "$CFG")
+    MINOR=$(jq .minor <<< "$CFG")
+    jq ".graphics.gl |= [$CFG] | .graphics.gles = []" dummy_plug.json > dummy_plug_core_$MAJOR$MINOR.json
+done
+
+for CFG in $ES_CONFIGS; do
+    MAJOR=$(jq .major <<< "$CFG")
+    MINOR=$(jq .minor <<< "$CFG")
+    jq ".graphics.gles |= [$CFG] | .graphics.gl = [] | .graphics.software_renderer = \"angle\"" dummy_plug.json > dummy_plug_es_$MAJOR$MINOR.json
+done
