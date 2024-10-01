bf95440a3b57bd527713d8d447c71a25fb36b61a
Håvard Bjerke <h0wy36@gmail.com>
2024-10-02 00:00:06 +0200

    github: Styling for generated test page

--- .github/tests/generate_test_page.sh
+++ .github/tests/generate_test_page.sh
@@ -12,17 +12,18 @@ echo '<html>
 <head>
     <title>Test results!</title>
 </head>
-<body>' > $OUT_FILE
+<body style="background-color: black; color: white;">' > $OUT_FILE
 
 for TEST_VARIANT in $TEST_VARIANTS; do
     echo $ARTIFACT_DIR/$TEST_VARIANT
     echo "<h1>$TEST_VARIANT</h1>" >> $OUT_FILE
     for IMG in $(find $ARTIFACT_DIR/$TEST_VARIANT/ -name *.jpg); do
         echo "<h3>$(basename $IMG)</h3>" >> $OUT_FILE
-        echo "<img src=\"data:image/jpeg;base64,$(base64 $IMG)\"/>" >> $OUT_FILE
+        echo "<img src=\"data:image/jpeg;base64,$(base64 --wrap=0 $IMG)\"/>" >> $OUT_FILE
     done
     for LOG in $(find $ARTIFACT_DIR/$TEST_VARIANT/ -name *.log); do
-        echo "<textarea rows=\"20\" cols=\"80\">$(cat $LOG)</textarea>" >> $OUT_FILE
+        echo "<h3>$(basename $LOG)</h3>" >> $OUT_FILE
+        echo "<textarea style=\"background-color: black; color: white;\" rows=\"20\" cols=\"80\">$(cat $LOG)</textarea>" >> $OUT_FILE
     done
 done
 
