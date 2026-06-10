5aa74ee74fc254f713c619d58e02ba26996c45bc
Håvard Bjerke <h0wy36@gmail.com>
2026-06-10 02:49:59 +0200

    toolchain: Suppress annoying ABI warning

--- toolchain/cmake/Toolchains/arm-buildroot-linux-gnueabihf.toolchain.cmake
+++ toolchain/cmake/Toolchains/arm-buildroot-linux-gnueabihf.toolchain.cmake
@@ -6,7 +6,9 @@ include(${CMAKE_CURRENT_LIST_DIR}/common/toolchain-prefix.cmake)
 set(CMAKE_C_COMPILER "${TOOLCHAIN_ROOT}/bin/${TOOLCHAIN_TOOL}gcc")
 set(CMAKE_CXX_COMPILER "${TOOLCHAIN_ROOT}/bin/${TOOLCHAIN_TOOL}g++")
 
-add_compile_options(-mtune=cortex-a8 -march=armv7-a+simd+vfpv3 -mfloat-abi=hard)
+# -Wno-psabi silences GCC 7.1 "parameter passing ... changed" notes; the ABI
+# change is internal and consistent across this toolchain, so it's harmless here.
+add_compile_options(-mtune=cortex-a8 -march=armv7-a+simd+vfpv3 -mfloat-abi=hard -Wno-psabi)
 add_link_options(
   -mtune=cortex-a8 -march=armv7-a+simd+vfpv3 -mfloat-abi=hard
   # -static-libstdc++ ${CMAKE_SYSROOT}/usr/lib/libc.a
