5ac9010b93c1e4fdaa3e2fba53555a544922ff91
Håvard Bjerke <h0wy36@gmail.com>
2026-07-05 11:44:19 +0200

    gleam: Remove gl2ext.h, it's broken
    Should not be re-enabled since GLAD is easier to work with

--- src/coffee/graphics/apis/gleam/include/glw/base.h
+++ src/coffee/graphics/apis/gleam/include/glw/base.h
@@ -126,7 +126,7 @@ inline void capture_gl_trace_data(
 #if C_HAS_INCLUDE(<GLES3/gl3ext.h>)
 #include <GLES3/gl3ext.h>
 #elif C_HAS_INCLUDE(<GLES2/gl2ext.h>)
-#include <GLES2/gl2ext.h>
+// #include <GLES2/gl2ext.h>
 #endif // C_HAS_INCLUDE(<GLES3/gl3ext.h>)
 
 #else // defined(GLEAM_USE_LINKED)
