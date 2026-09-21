38ab7d818071f5df9a4890fe064902fdd8fbc746
Håvard Bjerke <h0wy36@gmail.com>
2026-09-21 03:20:05 +0200

    blam: Fix oopsie

---
+++ examples/blam/cblam-testing/graphics_api_fwd.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <coffee/graphics/apis/gleam/rhi_draw_command.h>
+
+namespace gleam {
+
+struct texture_t;
+struct texture_2d_t;
+struct sampler_t;
+struct buffer_t;
+struct rendertarget_t;
+
+}
+
+namespace gfx {
+
+using namespace gleam;
+
+}
