d29bb0f372e490647b012b12e552d5f7f3c21b04
Håvard Bjerke <h0wy36@gmail.com>
2026-09-24 22:20:40 +0200

    emscripten: Remove dead ControllerInput
    Not needed after SDL3 fixed controller input

--- src/coffee/emscripten/include/coffee/emscripten_comp/emscripten_components.h
+++ src/coffee/emscripten/include/coffee/emscripten_comp/emscripten_components.h
@@ -55,33 +55,6 @@ struct GLContext
     entity_container* m_container = nullptr;
 };
 
-struct ControllerInput
-    : comp_app::interfaces::ControllerInput
-    , comp_app::AppService<ControllerInput, comp_app::ControllerInput>
-{
-    virtual ~ControllerInput() = default;
-
-    virtual void start_frame(
-        comp_app::detail::ContainerProxy& c,
-        const comp_app::detail::time_point&) final;
-
-    virtual libc_types::u32       count() const final;
-    virtual controller_map        state(libc_types::u32 idx) const final;
-    virtual comp_app::text_type_t name(libc_types::u32 idx) const final;
-
-  private:
-    struct state_t
-    {
-        std::string    name{};
-        controller_map state{};
-    };
-
-    std::map<libc_types::u32, state_t> m_controllers;
-
-    libc_types::i16 m_deadzone;
-    libc_types::f32 m_scale;
-};
-
 struct BatteryProvider
     : comp_app::interfaces::BatteryProvider
     , comp_app::AppService<BatteryProvider, comp_app::BatteryProvider>
